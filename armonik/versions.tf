067ff78521504ed9a00c317fe53b4c37956f2fad
lzianekhodja <lzianekhodja@aneo.fr>
2023-06-12 15:53:56 +0200

    tflint: for armonik

--- armonik/versions.tf
+++ armonik/versions.tf
@@ -5,13 +5,29 @@ terraform {
       source  = "gavinbunney/kubectl"
       version = ">= 1.14.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.21.1"
+    }
     helm = {
       source  = "hashicorp/helm"
-      version = ">= 2.0.0"
+      version = ">= 2.7.0"
     }
     pkcs12 = {
       source  = "chilicat/pkcs12"
       version = ">= 0.0.7"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.4.3"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = ">= 2.1.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = ">= 4.0.4"
     }
   }
 }

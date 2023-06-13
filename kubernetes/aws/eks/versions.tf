72cf8970fd3b0f16b0953e55e47887793cfdac90
lzianekhodja <lzianekhodja@aneo.fr>
2023-06-13 16:56:51 +0200

    delete action for tfupdate

--- kubernetes/aws/eks/versions.tf
+++ kubernetes/aws/eks/versions.tf
@@ -19,7 +19,7 @@ terraform {
     }
     random = {
       source  = "hashicorp/random"
-      version = ">= 3.4.3"
+      version = ">= 3.5.1"
     }
     local = {
       source  = "hashicorp/local"

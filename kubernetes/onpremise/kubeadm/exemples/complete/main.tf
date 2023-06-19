da61c9c355300a82f57bf3ff47b91047ff8927ac
alouvion-aneo <113617172+alouvion-aneo@users.noreply.github.com>
2023-06-19 19:42:12 +0200

    rename variable

--- kubernetes/onpremise/kubeadm/exemples/complete/main.tf
+++ kubernetes/onpremise/kubeadm/exemples/complete/main.tf
@@ -4,6 +4,7 @@ locals {
   master = {
     name                = "worker-1"
     public_dns          = "ec2-3-8-5-81.master.eu-west-2.compute.amazonaws.com"
+    private_dns          = "10.0.0.1"
     tls_private_key_pem = "my master node ssh private key"
 
   }
@@ -33,7 +34,7 @@ module "install_kubeadm_cluster" {
   source              = "../.."
   master_public_ip    = local.master.public_dns
   master_private_ip   = local.master.private_dns
-  tls_private_key_pem = local.master.private_key_pem
+  tls_private_key_pem = local.master.tls_private_key_pem
   user                = local.user
   kubeadm_token       = local.token
   cni_pluggin         = "flannel" # calico or flannel

b4499c1942da43fe53055673ee2221cbbe507a0e
YKharouni <ykharouni@aneo.fr>
2023-06-27 16:06:12 +0200

    add map of objects

--- container-registry/aws/ecr/main.tf
+++ container-registry/aws/ecr/main.tf
@@ -13,6 +13,42 @@ resource "aws_ecr_repository" "ecr" {
   force_delete = true #(Optional) If true, will delete the repository even if it contains images. Defaults to false.
 }
 
+# IAM policy
+
+data "aws_iam_policy_document" "iam-ecr" {
+  statement {
+    sid    = "new policy"
+    effect = "Allow"
+
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+
+    actions = [
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:BatchGetImage",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:PutImage",
+      "ecr:InitiateLayerUpload",
+      "ecr:UploadLayerPart",
+      "ecr:CompleteLayerUpload",
+      "ecr:DescribeRepositories",
+      "ecr:GetRepositoryPolicy",
+      "ecr:ListImages",
+      "ecr:DeleteRepository",
+      "ecr:BatchDeleteImage",
+      "ecr:SetRepositoryPolicy",
+      "ecr:DeleteRepositoryPolicy",
+    ]
+  }
+}
+
+resource "aws_ecr_repository_policy" "ecr-policy" {
+  repository = aws_ecr_repository.ecr.name
+  policy     = data.aws_iam_policy_document.iam-ecr.json
+}
+
 # Copy images
 resource "null_resource" "copy_images" {
   count = length(var.repositories)

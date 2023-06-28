3a407307a3af6b81167254505221be7031687c54
ABLA-Adem-ANEO <aabla@aneo.fr>
2023-06-28 12:15:24 +0200

    add version to kub providers example

--- storage/onpremise/activemq/exemple/versions.tf
+++ storage/onpremise/activemq/exemple/versions.tf
@@ -1,8 +1,13 @@
 terraform {
   required_version = ">= 1.0"
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.21.1"
+    }
+  }
 }
 provider "kubernetes" {
   config_path    = "/home/adem/.kube/config"
   config_context = "default"
-
 }

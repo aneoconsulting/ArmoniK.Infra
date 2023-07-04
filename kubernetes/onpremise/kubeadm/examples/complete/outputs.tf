7a4279d1e3780862df0cf48265c41f0123d3e735
alouvion-aneo <113617172+alouvion-aneo@users.noreply.github.com>
2023-07-04 13:04:57 +0200

    comments updated

--- kubernetes/onpremise/kubeadm/examples/complete/outputs.tf
+++ kubernetes/onpremise/kubeadm/examples/complete/outputs.tf
@@ -1,9 +1,9 @@
-output "kubeconfig_file" {
+output "kubeconfig_file_export_help" {
   description = "Use this export to begin to use your cluster"
   value       = "export KUBECONFIG=$HOME/.kube/config_remote"
 }
 
-output "kubeconfig_file" {
+output "display_nodes_command_jelp" {
   description = "A sample command to display nodes of your cluster"
   value       = "kubectl --kubeconfig $HOME/.kube/config_remote get nodes"
 }

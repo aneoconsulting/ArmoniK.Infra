dfb7aaaafa76a2dd277fdb05d5b50189c6d3926a
ABLA-Adem-ANEO <aabla@aneo.fr>
2023-07-05 16:50:16 +0200

    correct local.tf

--- storage/onpremise/activemq/locals.tf
+++ storage/onpremise/activemq/locals.tf
@@ -1,7 +1,7 @@
 locals {
   # ActiveMQ node selector
-  node_selector_keys    = keys(var.activemq.node_selector)
-  node_selector_values  = values(var.activemq.node_selector)
+  node_selector_keys    = keys(var.node_selector)
+  node_selector_values  = values(var.node_selector)
   adapter_class_name    = "ArmoniK.Core.Adapters.Amqp.QueueBuilder"
   adapter_absolute_path = "/adapters/queue/amqp/ArmoniK.Core.Adapters.Amqp.dll"
   engine_type           = "ActiveMQ"

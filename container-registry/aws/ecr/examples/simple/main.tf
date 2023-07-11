eb77802c2db5fc284063a01975cc60bd938b7332
YKharouni <ykharouni@aneo.fr>
2023-07-11 18:49:38 +0200

    add examples

---
+++ container-registry/aws/ecr/examples/simple/main.tf
@@ -0,0 +1,41 @@
+# Current account
+data "aws_caller_identity" "current" {}
+
+# this external provider is used to get date during the plan step.
+data "external" "static_timestamp" {
+  program = ["date", "+{ \"creation_date\": \"%Y/%m/%d %T\" }"]
+}
+
+# this resource is just used to prevent change of the creation_date during successive 'terraform apply'
+resource "null_resource" "timestamp" {
+  triggers = {
+    creation_date = data.external.static_timestamp.result.creation_date
+  }
+  lifecycle {
+    ignore_changes = [triggers]
+  }
+}
+
+locals {
+  repositories = {
+    armonik-polling-agent = {
+      image = "dockerhubaneo/armonik_pollingagent"
+      tag   = "0.14.3"
+    }
+  }
+  new_repositories = { for k, v in local.repositories : "test/${k}" => v }
+}
+
+# AWS ECR
+module "ecr" {
+  source       = "../../../ecr"
+  aws_profile  = var.aws_profile
+  repositories = local.new_repositories
+  tags = {
+    env             = "test"
+    app             = "complete"
+    module          = "AWS ECR"
+    "create by"     = data.aws_caller_identity.current.arn
+    "creation date" = null_resource.timestamp.triggers["creation_date"]
+  }
+}
--- container-registry/aws/ecr/examples/simple/outputs.tf
+++ container-registry/aws/ecr/examples/simple/outputs.tf
@@ -1,9 +1,9 @@
 output "repositories" {
   description = "Map of ECR repositories"
-  value       = { for repo in aws_ecr_repository.ecr : repo => repo.repository_url }
+  value       = module.ecr.repositories
 }
 
 output "kms_key_id" {
   description = "ARN of KMS used for ECR"
-  value       = var.kms_key_id
+  value       = module.ecr.kms_key_id
 }

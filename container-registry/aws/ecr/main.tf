39ed9c074334a2e68aab0c5beea608fa16d8a777
YKharouni <ykharouni@aneo.fr>
2023-07-11 18:13:04 +0200

    add ecr module

--- container-registry/aws/ecr/main.tf
+++ container-registry/aws/ecr/main.tf
@@ -1,7 +1,20 @@
+# Current account
+data "aws_caller_identity" "current" {}
+
+# Current AWS region
+data "aws_region" "current" {}
+
+locals {
+  mutability = var.mutability
+  region     = data.aws_region.current.name
+  tags       = merge({ module = "ecr" }, var.tags)
+}
+
 # create ECR repositories
 resource "aws_ecr_repository" "ecr" {
-  count = length(var.repositories)
-  name  = var.repositories[count.index].name
+  for_each             = var.repositories
+  name                 = each.key
+  image_tag_mutability = local.mutability
   image_scanning_configuration {
     scan_on_push = true
   }
@@ -13,9 +26,34 @@ resource "aws_ecr_repository" "ecr" {
   force_delete = true #(Optional) If true, will delete the repository even if it contains images. Defaults to false.
 }
 
-# IAM policy
+# lifecycle policy
+resource "aws_ecr_lifecycle_policy" "ecr_lifecycle_policy" {
+  for_each   = aws_ecr_repository.ecr
+  repository = each.key
 
-data "aws_iam_policy_document" "iam-ecr" {
+  policy = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Expire images older than 14 days",
+            "selection": {
+                "tagStatus": "untagged",
+                "countType": "sinceImagePushed",
+                "countUnit": "days",
+                "countNumber": 14
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+}
+EOF
+}
+
+# IAM policy
+data "aws_iam_policy_document" "iam_ecr" {
   statement {
     sid    = "new policy"
     effect = "Allow"
@@ -45,59 +83,22 @@ data "aws_iam_policy_document" "iam-ecr" {
 }
 
 resource "aws_ecr_repository_policy" "ecr_policy" {
-  repository = aws_ecr_repository.ecr.name
-  policy     = data.aws_iam_policy_document.iam-ecr.json
+  for_each   = aws_ecr_repository.ecr
+  repository = each.key
+  policy     = data.aws_iam_policy_document.iam_ecr.json
 }
-
-# Copy images
-/*resource "null_resource" "copy_images" {
-  count = length(var.repositories)
-  triggers = {
-    state = join("-", [
-      var.repositories[count.index].name, var.repositories[count.index].image, var.repositories[count.index].tag
-    ])
-  }
-  provisioner "local-exec" {
-    command = <<-EOT
-aws ecr get-login-password --profile ${var.profile} --region ${local.region}  | docker login --username AWS --password-stdin ${data.aws_caller_identity.current.account_id}.dkr.ecr.${local.region}.amazonaws.com
-aws ecr-public get-login-password --profile ${var.profile} --region us-east-1  | docker login --username AWS --password-stdin public.ecr.aws
-if [ -z "$(docker images -q '${var.repositories[count.index].image}:${var.repositories[count.index].tag}')" ]
-then
-  if ! docker pull ${var.repositories[count.index].image}:${var.repositories[count.index].tag}
-  then
-    echo "cannot download image ${var.repositories[count.index].image}:${var.repositories[count.index].tag}"
-    exit 1
-  fi
-fi
-if ! docker tag ${var.repositories[count.index].image}:${var.repositories[count.index].tag} ${data.aws_caller_identity.current.account_id}.dkr.ecr.${local.region}.amazonaws.com/${var.repositories[count.index].name}:${var.repositories[count.index].tag}
-then
-  echo "cannot tag image ${var.repositories[count.index].image}:${var.repositories[count.index].tag} to ${data.aws_caller_identity.current.account_id}.dkr.ecr.${local.region}.amazonaws.com/${var.repositories[count.index].name}:${var.repositories[count.index].tag}"
-  exit 1
-fi
-if ! docker push ${data.aws_caller_identity.current.account_id}.dkr.ecr.${local.region}.amazonaws.com/${var.repositories[count.index].name}:${var.repositories[count.index].tag}
-then
-  echo "cannot push image ${data.aws_caller_identity.current.account_id}.dkr.ecr.${local.region}.amazonaws.com/${var.repositories[count.index].name}:${var.repositories[count.index].tag}"
-  exit 1
-fi
-EOT
-  }
-  depends_on = [
-    aws_ecr_repository.ecr
-  ]
-}*/
-
 # Copy images
 resource "null_resource" "copy_images" {
   for_each = var.repositories
   triggers = {
     state = join("-", [
-      var.repositories[each.key], var.repositories[each.key].image, var.repositories[each.key].tag
+      each.key, var.repositories[each.key].image, var.repositories[each.key].tag
     ])
   }
   provisioner "local-exec" {
     command = <<-EOT
-aws ecr get-login-password --profile ${var.profile} --region ${local.region}  | docker login --username AWS --password-stdin ${data.aws_caller_identity.current.account_id}.dkr.ecr.${local.region}.amazonaws.com
-aws ecr-public get-login-password --profile ${var.profile} --region us-east-1  | docker login --username AWS --password-stdin public.ecr.aws
+aws ecr get-login-password --profile ${var.aws_profile} --region ${local.region}  | docker login --username AWS --password-stdin ${data.aws_caller_identity.current.account_id}.dkr.ecr.${local.region}.amazonaws.com
+aws ecr-public get-login-password --profile ${var.aws_profile} --region us-east-1  | docker login --username AWS --password-stdin public.ecr.aws
 if [ -z "$(docker images -q '${var.repositories[each.key].image}:${var.repositories[each.key].tag}')" ]
 then
   if ! docker pull ${var.repositories[each.key].image}:${var.repositories[each.key].tag}
@@ -106,14 +107,14 @@ then
     exit 1
   fi
 fi
-if ! docker tag ${var.repositories[each.key].image}:${var.repositories[each.key].tag} ${data.aws_caller_identity.current.account_id}.dkr.ecr.${local.region}.amazonaws.com/${var.repositories[each.key].name}:${var.repositories[each.key].tag}
+if ! docker tag ${var.repositories[each.key].image}:${var.repositories[each.key].tag} ${data.aws_caller_identity.current.account_id}.dkr.ecr.${local.region}.amazonaws.com/${each.key}:${var.repositories[each.key].tag}
 then
-  echo "cannot tag image ${var.repositories[each.key].image}:${var.repositories[each.key].tag} to ${data.aws_caller_identity.current.account_id}.dkr.ecr.${local.region}.amazonaws.com/${var.repositories[each.key]}:${var.repositories[each.key].tag}"
+  echo "cannot tag image ${var.repositories[each.key].image}:${var.repositories[each.key].tag} to ${data.aws_caller_identity.current.account_id}.dkr.ecr.${local.region}.amazonaws.com/${each.key}:${var.repositories[each.key].tag}"
   exit 1
 fi
-if ! docker push ${data.aws_caller_identity.current.account_id}.dkr.ecr.${local.region}.amazonaws.com/${var.repositories[each.key]}:${var.repositories[each.key].tag}
+if ! docker push ${data.aws_caller_identity.current.account_id}.dkr.ecr.${local.region}.amazonaws.com/${each.key}:${var.repositories[each.key].tag}
 then
-  echo "cannot push image ${data.aws_caller_identity.current.account_id}.dkr.ecr.${local.region}.amazonaws.com/${var.repositories[each.key]}:${var.repositories[each.key].tag}"
+  echo "cannot push image ${data.aws_caller_identity.current.account_id}.dkr.ecr.${local.region}.amazonaws.com/${each.key}:${var.repositories[each.key].tag}"
   exit 1
 fi
 EOT

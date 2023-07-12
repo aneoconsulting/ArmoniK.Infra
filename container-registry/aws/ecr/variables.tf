ad77864a33a505dbb85a815c4b8d6d3d202ad6ba
YKharouni <ykharouni@aneo.fr>
2023-07-12 11:09:18 +0200

    clean code

--- container-registry/aws/ecr/variables.tf
+++ container-registry/aws/ecr/variables.tf
@@ -1,6 +1,6 @@
 # Profile
 variable "aws_profile" {
-  description = "Profile of AWS credentials to deploy Terraform sources"
+  description = "AWS Profile used to login and push container images on ECR"
   type        = string
 }
 
@@ -15,12 +15,12 @@ variable "tags" {
 variable "kms_key_id" {
   description = "KMS to encrypt ECR repositories"
   type        = string
-  default     = ""
+  default     = null
 }
 
 # Variable to enable mutability
 variable "mutability" {
-  description = "Variable to enable mutability"
+  description = "The tag mutability setting for the repository"
   type        = string
   default     = "MUTABLE"
   validation {
@@ -31,10 +31,32 @@ variable "mutability" {
 
 # List of ECR repositories to create
 variable "repositories" {
-  description = "List of ECR repositories to create"
+  description = "Map of ECR repositories to create. Each repository is an object of \"image\" and \"tag\" parameters"
   type = map(object({
     image = string
     tag   = string
   }))
   default = {}
+}
+
+variable "scan_on_push" {
+  description = " Indicates whether images are scanned after being pushed to the repository or not scanned"
+  type        = bool
+  default     = null
+}
+
+variable "force_delete" {
+  description = "If true, will delete the repository even if it contains images."
+  type        = bool
+  default     = true
+}
+
+variable "encryption_type" {
+  description = "The encryption type to use for the repository."
+  type        = string
+  default     = "AES256"
+  validation {
+    condition     = contains(["AES256", "KMS"], var.encryption_type)
+    error_message = "Valid values for \"encryption_type\" are: \"AES256\" | \"KMS\"."
+  }
 }

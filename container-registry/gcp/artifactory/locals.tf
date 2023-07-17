6c85f5ed2eb17022a2a5cd31982161667f297e5e
hbitoun-aneo <hbitoun@aneo.fr>
2023-07-17 11:31:36 +0200

    fix: changes to create only one registry at once

--- container-registry/gcp/artifactory/locals.tf
+++ container-registry/gcp/artifactory/locals.tf
@@ -11,18 +11,6 @@
 
 
 locals {
-  # flatten ensures that this local value is a flat list of objects, rather
-  # than a list of lists of objects.
-  repository_images = flatten([
-    for repository in var.repositories : [
-      for image in repository.images : {
-        repository_name     = repository.artifact_repository_name
-        image_name          = image.image
-        image_tag           = image.tag
-      }
-    ]
-  ])
-  
   location = (var.zone == null || var.zone == "") ? var.region : "${var.region}-${var.zone}"
 }
 

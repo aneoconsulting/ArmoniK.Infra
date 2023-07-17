bc26e7acf817ba53ba3847e68858f209376a1ab7
YKharouni <ykharouni@aneo.fr>
2023-07-17 09:49:24 +0200

    remove locals.tf file

--- storage/aws/s3/locals.tf
+++
@@ -1,4 +0,0 @@
-locals {
-  attach_policy = (var.s3.attach_require_latest_tls_policy || var.s3.attach_deny_insecure_transport_policy || var.s3.attach_policy)
-  tags          = merge(var.tags, { module = "s3" })
-}

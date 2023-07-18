f9550bfe542c1460f9b4ce06fa359115f5593415
alouvion-aneo <113617172+alouvion-aneo@users.noreply.github.com>
2023-07-18 14:14:18 +0200

    expose validity_period_hours_variable_tls_certificate_for_mongo

--- storage/onpremise/mongodb/certificates.tf
+++ storage/onpremise/mongodb/certificates.tf
@@ -10,7 +10,7 @@ resource "tls_private_key" "root_mongodb" {
 resource "tls_self_signed_cert" "root_mongodb" {
   private_key_pem       = tls_private_key.root_mongodb.private_key_pem
   is_ca_certificate     = true
-  validity_period_hours = "168"
+  validity_period_hours = var.validity_period_hours
   allowed_uses = [
     "cert_signing",
     "key_encipherment",

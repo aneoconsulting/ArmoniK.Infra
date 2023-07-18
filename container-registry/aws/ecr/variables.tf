5e6914422a78477bfdb035b4d1d26f7b3c114020
lzianekhodja <lzianekhodja@aneo.fr>
2023-07-18 17:39:03 +0200

    add lifecycle policy

--- container-registry/aws/ecr/variables.tf
+++ container-registry/aws/ecr/variables.tf
@@ -32,7 +32,7 @@ variable "mutability" {
 # List of ECR repositories to create
 variable "repositories" {
   description = "Map of ECR repositories to create. Each repository is an object of \"image\" and \"tag\" parameters"
-  type = map(object({
+  type        = map(object({
     image = string
     tag   = string
   }))
@@ -72,3 +72,11 @@ variable "push_and_pull_accounts" {
   type        = list(string)
   default     = []
 }
+
+variable "lifecycle_policy" {
+  description = "Manages an ECR repository lifecycle policy"
+  type        = map(any)
+  default     = null
+}
+
+

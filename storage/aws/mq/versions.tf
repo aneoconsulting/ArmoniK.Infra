77aecfe3aa8449c03b65fee917c0f163c58e12b3
YKharouni <ykharouni@aneo.fr>
2023-07-25 16:14:21 +0200

    update aws/mq module

--- storage/aws/mq/versions.tf
+++ storage/aws/mq/versions.tf
@@ -3,11 +3,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.3.0"
+      version = ">= 5.4.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = ">= 3.4.3"
+      version = "~> 3.5.1"
     }
   }
 }

2f815048e05978846fc6c5cf22f81c3b7aa47f92
YKharouni <ykharouni@aneo.fr>
2023-07-28 17:29:23 +0200

    clean code

--- storage/aws/elasticache/variables.tf
+++ storage/aws/elasticache/variables.tf
@@ -31,8 +31,8 @@ variable "vpc_subnet_ids" {
 # AWS Elasticache
 variable "engine" {
   description = "Name of the cache engine to be used for the clusters in this replication group"
-  type = string
-  default = "redis"
+  type        = string
+  default     = "redis"
   validation {
     condition     = contains(["redis"], var.engine)
     error_message = "The only valid value for \"engine\" : \"redis\" ."
@@ -41,79 +41,78 @@ variable "engine" {
 
 variable "engine_version" {
   description = "Version number of the cache engine to be used for the cache clusters in this replication group"
-  type = string
-  default = "engine_version_actual"
+  type        = string
+  default     = "engine_version_actual"
 }
 
 variable "node_type" {
   description = "Instance class to be used"
-  type = string
-  default = "cache.r4.large"
+  type        = string
+  default     = "cache.r4.large"
 }
 
 variable "apply_immediately" {
   description = "Specifies whether any modifications are applied immediately, or during the next maintenance window"
-  type = bool
-  default = false
+  type        = bool
+  default     = false
 }
 
 variable "multi_az_enabled" {
   description = "Specifies whether to enable Multi-AZ Support for the replication group"
-  type = bool
-  default = false
+  type        = bool
+  default     = false
 }
 
 variable "automatic_failover_enabled" {
   description = "Specifies whether a read-only replica will be automatically promoted to read/write primary if the existing primary fails"
-  type = bool
-  default = false
+  type        = bool
+  default     = false
 }
 
 variable "num_cache_clusters" {
   description = "Number of cache clusters (primary and replicas) this replication group will have"
-  type = number
-  default = 1
+  type        = number
+  default     = 1
 }
 
 variable "preferred_cache_cluster_azs" {
   description = "List of EC2 availability zones in which the replication group's cache clusters will be created"
-  type = list(string)
-  default = []
+  type        = list(string)
+  default     = []
 }
 
 variable "data_tiering_enabled" {
   description = "Enables data tiering"
-  type  = bool
-  default = false
+  type        = bool
+  default     = false
 }
 
 variable "log_retention_in_days" {
   description = "Number of days of the retention of the logs"
-  type = number
-  default = 0
+  type        = number
+  default     = 0
 }
 
 variable "slow_log" {
-  description = ""
-  type = string
-  default = ""
+  description = "Slow log"
+  type        = string
+  default     = ""
 }
 
 variable "engine_log" {
   description = "Engine type of the logs"
-  type = string
-  default = ""
+  type        = string
+  default     = ""
 }
 
 variable "kms_key_id" {
   description = "AWS KMS key id"
-  type = string
-  default = null
+  type        = string
+  default     = null
 }
 
 variable "log_kms_key_id" {
   description = "AWS KMS key id for logs"
-  type = string
-  default = null
+  type        = string
+  default     = null
 }
-

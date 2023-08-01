ce072f17e6e6aed62ae6e2032df5393c302a6ab8
hbitoun-aneo <hbitoun@aneo.fr>
2023-08-01 11:43:20 +0200

    fix: change complex to complete example + change location to data google client config + add provider google and associated vars inside examples

--- container-registry/gcp/artifactory/examples/simple/versions.tf
+++ container-registry/gcp/artifactory/examples/simple/versions.tf
@@ -3,15 +3,12 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 4.51.0"
+      version = ">= 4.75.0"
     }
   }
 }
 
-
 provider "google" {
-  credentials = file(var.credentials_file)
-  project     = var.project_id
-  region      = var.region
-  zone        = var.zone
+  project = var.project
+  region  = var.region
 }

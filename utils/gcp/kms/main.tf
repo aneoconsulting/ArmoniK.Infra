28bd1ebb48ab2275aa184dc78f1179a1c6b61b8c
hbitoun-aneo <hbitoun@aneo.fr>
2023-08-03 15:45:06 +0200

    fix: add simple example and remove unused vars

--- utils/gcp/kms/main.tf
+++ utils/gcp/kms/main.tf
@@ -1,16 +1,10 @@
 
 data "google_client_config" "current" {}
 
-locals {
-  kms_crypto_key_ring = "/projects/${data.google_client_config.current.project}/locations/${data.google_client_config.current.region}/keyRings/${var.kms_crypto_key_ring}"
-}
-
-
 ###### SECTION - IAM POLICY KMS
 
 resource "google_kms_crypto_key_iam_policy" "crypto_key_iam_policy" {
-  depends_on = [google_kms_crypto_key.kms_crypto_key]
-  for_each   = var.google_kms_crypto_key_iam_policy_data != null ? var.google_kms_crypto_key_iam_policy_data : {}
+  count = var.google_kms_crypto_key_iam_policy_data != null ? 1 : 0
 
   crypto_key_id = google_kms_crypto_key.kms_crypto_key.id
   policy_data   = var.google_kms_crypto_key_iam_policy_data
@@ -20,7 +14,7 @@ resource "google_kms_crypto_key_iam_policy" "crypto_key_iam_policy" {
 
 resource "google_kms_key_ring_iam_policy" "key_ring_iam_policy" {
   depends_on = [google_kms_key_ring.key_ring]
-  for_each   = var.google_kms_key_ring_iam_policy_data != null ? var.google_kms_key_ring_iam_policy_data : {}
+  count      = var.google_kms_key_ring_iam_policy_data != null ? 1 : 0
 
   key_ring_id = google_kms_key_ring.key_ring.id
   policy_data = var.google_kms_key_ring_iam_policy_data
@@ -30,7 +24,7 @@ resource "google_kms_key_ring_iam_policy" "key_ring_iam_policy" {
 ###### SECTION - KMS KEY RING
 
 resource "google_kms_key_ring" "key_ring" {
-  name     = var.google_kms_key_ring_name
+  name     = var.kms_key_ring_name
   location = data.google_client_config.current.region
   project  = data.google_client_config.current.project
 }
@@ -38,14 +32,11 @@ resource "google_kms_key_ring" "key_ring" {
 ###### SECTION - KMS CRYPTO KEY
 
 resource "google_kms_crypto_key" "kms_crypto_key" {
-  depends_on = [google_kms_key_ring.key_ring]
-
   name                          = var.kms_crypto_key_name
   key_ring                      = google_kms_key_ring.key_ring.id
   rotation_period               = var.kms_crypto_key_rotation_period
   labels                        = var.kms_crypto_key_labels
   purpose                       = var.kms_crypto_key_purpose
-  destroy_scheduled_duration    = var.kms_crypto_key_destroy_scheduled_duration
   import_only                   = var.kms_crypto_key_import_only
   skip_initial_version_creation = var.kms_crypto_key_skip_initial_version_creation
 
@@ -58,6 +49,10 @@ resource "google_kms_crypto_key" "kms_crypto_key" {
     }
   }
 
+  lifecycle {
+    prevent_destroy = true
+  }
+
 }
 
 ###### SECTION - KMS CRYPTO KEY VERSION
@@ -65,71 +60,25 @@ resource "google_kms_crypto_key" "kms_crypto_key" {
 resource "google_kms_crypto_key_version" "kms_crypto_key_version" {
   depends_on = [google_kms_crypto_key.kms_crypto_key]
 
-  crypto_key       = google_kms_crypto_key.kms_crypto_key.id
-  state            = var.kms_crypto_key_version_state
-  name             = var.kms_crypto_key_version_name
-  id               = var.kms_crypto_key_version_id
-  protection_level = var.kms_crypto_key_version_protection_level
-  generate_time    = var.kms_crypto_key_version_generate_time
-  algorithm        = var.kms_crypto_key_version_algorithm
-
-  dynamic "attestion" {
-    for_each = var.kms_crypto_key_version_attestation != null ? [var.kms_crypto_key_version_attestation] : []
-    content {
-      format  = var.kms_crypto_key_version_attestation.value["format"]
-      content = var.kms_crypto_key_version_attestation.value["content"]
-      cert_chains {
-        cavium_certs           = var.kms_crypto_key_version_attestation.value["cert_chains"]["cavium_certs"]
-        google_card_certs      = var.kms_crypto_key_version_attestation.value["cert_chains"]["google_card_certs"]
-        google_partition_certs = var.kms_crypto_key_version_attestation.value["cert_chains"]["google_partition_certs"]
-      }
-      external_protection_level_options {
-        external_key_uri        = var.kms_crypto_key_version_attestation.value["external_protection_level_options"]["external_key_uri"]
-        ekm_connection_key_path = var.kms_crypto_key_version_attestation.value["external_protection_level_options"]["ekm_connection_key_path"]
-      }
-    }
-  }
+  crypto_key = google_kms_crypto_key.kms_crypto_key.id
+  state      = var.kms_crypto_key_version_state
 }
-
 
 ###### SECTION - KMS RING IMPORT JOB
 
 resource "google_kms_key_ring_import_job" "kms_key_ring_import_job" {
-  count            = var.google_kms_key_ring_import_job_ID ? 1 : 0
-  key_ring         = google_kms_key_ring.keyring.id
-  import_job_id    = var.google_kms_key_ring_import_job_ID
+  count            = var.google_kms_key_ring_import_job_id != null ? 1 : 0
+  key_ring         = google_kms_key_ring.key_ring.id
+  import_job_id    = var.google_kms_key_ring_import_job_id
   import_method    = var.google_kms_key_ring_import_job_method
   protection_level = var.google_kms_key_ring_import_job_protection_level
-
-  id          = var.google_kms_key_ring_import_job_id
-  name        = var.google_kms_key_ring_import_job_name
-  expire_time = var.google_kms_key_ring_import_job_expire_time
-  state       = var.google_kms_key_ring_import_job_state
-
-  dynamic "public_key" {
-    for_each = var.google_kms_key_ring_import_job_public_key != null ? [var.google_kms_key_ring_import_job_public_key] : []
-    content {
-      pem = var.google_kms_key_ring_import_job_public_key.value["pem"]
-    }
-  }
-
-
-  dynamic "attestation" {
-    for_each = var.google_kms_key_ring_import_job_attestation != null ? [var.google_kms_key_ring_import_job_attestation] : []
-    content {
-      format  = var.google_kms_key_ring_import_job_attestation.value["format"]
-      content = var.google_kms_key_ring_import_job_attestation.value["content"]
-    }
-  }
-
 }
 
 ###### SECTION - KMS CIPHERTEXT
 
 resource "google_kms_secret_ciphertext" "ciphertext_password" {
   count                         = var.google_kms_secret_ciphertext_plaintext != null ? 1 : 0
-  crypto_key                    = google_kms_crypto_key.cryptokey.id
+  crypto_key                    = google_kms_crypto_key.kms_crypto_key.id
   plaintext                     = var.google_kms_secret_ciphertext_plaintext
   additional_authenticated_data = var.google_kms_secret_ciphertext_additional_authenticated_data
 }
-
--- utils/gcp/kms/variables.tf
+++ utils/gcp/kms/variables.tf
@@ -1,49 +1,36 @@
 # SECTION - KMS CRYPTO KEY
 
 variable "kms_crypto_key_name" {
-  description = 
-  type        = string
-}
-
-variable "kms_crypto_key_ring" {
-  description = 
+  description = ""
   type        = string
 }
 
 variable "kms_crypto_key_labels" {
-  description = 
+  description = ""
   type        = map(string)
-  default     = null 
+  default     = null
 }
 
 variable "kms_crypto_key_purpose" {
-  description = 
+  description = ""
   type        = string
-  default     = "ENCRYPT_DECRYPT" 
+  default     = "ENCRYPT_DECRYPT"
 }
 
 variable "kms_crypto_key_rotation_period" {
-  description = 
+  description = ""
   type        = string
-  default     = null 
+  default     = null
 }
 
 variable "kms_crypto_key_version_template" {
-  description = 
-  type        = object({
-    algorithm = string
+  description = ""
+  type = object({
+    algorithm        = string
     protection_level = optional(string)
   })
-  default     = null 
+  default = null
 }
-
-
-variable "kms_crypto_key_destroy_scheduled_duration" {
-  description = ""
-  type        = string
-  default     = "24h"
-}
-
 
 variable "kms_crypto_key_import_only" {
   description = ""
@@ -53,7 +40,7 @@ variable "kms_crypto_key_import_only" {
 
 variable "kms_crypto_key_skip_initial_version_creation" {
   description = ""
-  type        = bool 
+  type        = bool
   default     = false
 }
 
@@ -64,156 +51,60 @@ variable "kms_key_ring_name" {
   type        = string
 }
 
-variable "kms_key_ring_id" {
-  description = ""
-  type        = string
-}
-
 ###### SECTION - KMS CRYPTO KEY VERSION
-
-
-variable "kms_crypto_key_version_cyrpto_key" {
-  description = ""
-  type        = string
-}
 
 variable "kms_crypto_key_version_state" {
   description = ""
   type        = string
-  default     = null 
-}
-
-variable "kms_crypto_key_version_id" {
-  description = ""
-  type        = string
-}
-
-variable "kms_crypto_key_version_name" {
-  description = ""
-  type        = string
-  default     = null 
-}
-
-variable "kms_crypto_key_version_protection_level" {
-  description = ""
-  type        = string
-  default     = null 
-}
-
-variable "kms_crypto_key_version_generate_time" {
-  description = ""
-  type        = string
-  default     = null 
-}
-
-variable "kms_crypto_key_version_algorithm" {
-  description = ""
-  type        = string
-  default     = null 
-}
-
-variable "kms_crypto_key_version_attestation" {
-  description = ""
-  type        = object({
-    format  = optional(string)
-    content = optional(string)
-    cert_chains                       = object({
-      cavium_certs           = optional(string)
-      google_card_certs      = optional(string)
-      google_partition_certs = optional(string)
-    })
-    external_protection_level_options = object({
-      external_key_uri        = optional(string)
-      ekm_connection_key_path = optional(string)
-    })
-  })
-  default     = null 
+  default     = null
 }
 
 ###### SECTION - KMS RING IMPORT JOB
 
-variable "google_kms_key_ring_import_job_ID" {
+variable "google_kms_key_ring_import_job_id" {
   description = "It must be unique within a KeyRing."
   type        = string
   validation {
-    condition     = can(regex("[a-zA-Z0-9_-]{1,63}"))
-    error_message = "google_kms_key_ring_import_job_ID can only match the regular expression : [a-zA-Z0-9_-]{1,63}"
+    condition     = can(regex("[a-zA-Z0-9_-]{1,63}", var.google_kms_key_ring_import_job_id)) || var.google_kms_key_ring_import_job_id == null
+    error_message = "google_kms_key_ring_import_job_id can only match the regular expression : [a-zA-Z0-9_-]{1,63}"
   }
+  default = null
 }
 
 variable "google_kms_key_ring_import_job_method" {
   description = "The wrapping method to be used for incoming key material."
   type        = string
   validation {
-    condition     = can(regex("RSA_OAEP_3072_SHA1_AES_256|RSA_OAEP_4096_SHA1_AES_256"))
+    condition     = can(regex("RSA_OAEP_3072_SHA1_AES_256|RSA_OAEP_4096_SHA1_AES_256", var.google_kms_key_ring_import_job_method))
     error_message = "google_kms_key_ring_import_job_method can only take one of these values : RSA_OAEP_3072_SHA1_AES_256 or RSA_OAEP_4096_SHA1_AES_256"
   }
+  default = "RSA_OAEP_3072_SHA1_AES_256"
 }
 
 variable "google_kms_key_ring_import_job_protection_level" {
   description = "The protection level of the ImportJob."
   type        = string
   validation {
-    condition     = can(regex("SOFTWARE|HSM|EXTERNAL"))
+    condition     = can(regex("SOFTWARE|HSM|EXTERNAL", var.google_kms_key_ring_import_job_protection_level))
     error_message = "google_kms_key_ring_import_job_protection_level can only take one of these values : SOFTWARE or EXTERNAL or HSM"
   }
-}
-
-variable "google_kms_key_ring_import_job_id" {
-  description = "An identifier for the resource with format {{name}}"
-  type        = string
-  default = null
-}
-
-variable "google_kms_key_ring_import_job_name" {
-  description = "The resource name for this ImportJob"
-  type        = ""
-  default = null
-}
-
-variable "google_kms_key_ring_import_job_expire_time" {
-  description = "The time at which this resource is scheduled for expiration and can no longer be used. This is in RFC3339 text format"
-  type        = string
-  default = null
-}
-
-variable "google_kms_key_ring_import_job_state" {
-  description = "The current state of the ImportJob, indicating if it can be used."
-  type        = string
-  default = null
-}
-
-variable "google_kms_key_ring_import_job_public_key" {
-  description = "The public key with which to wrap key material prior to import."
-  type        = object({
-    pem = string
-  })
-  default = null
-}
-
-variable "google_kms_key_ring_import_job_attestation" {
-  description = "Statement that was generated and signed by the key creator (for example, an HSM) at key creation time."
-  type        = object({
-    format  = string
-    content = string
-  })
-  default = null
+  default = "SOFTWARE"
 }
 
 ###### SECTION - IAM POLICY KMS RING
 
 variable "google_kms_key_ring_iam_policy_data" {
   description = ""
-  type        = map(list(string))
-  default     = null 
+  type        = string
+  default     = null
 }
 
 ###### SECTION - IAM POLICY KMS
 
 variable "google_kms_crypto_key_iam_policy_data" {
   description = ""
-  type        = map(list(string))
-  default     = null 
+  type        = string
+  default     = null
 }
 
 ###### SECTION - KMS CIPHERTEXT
@@ -221,10 +112,11 @@ variable "google_kms_crypto_key_iam_policy_data" {
 variable "google_kms_secret_ciphertext_plaintext" {
   description = "The plaintext to be encrypted."
   type        = string
+  default     = null
 }
 
 variable "google_kms_secret_ciphertext_additional_authenticated_data" {
   description = "The additional authenticated data used for integrity checks during encryption and decryption."
   type        = string
-  default     = null 
+  default     = null
 }

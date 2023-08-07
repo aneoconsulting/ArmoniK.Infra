c210ea5ae71b3aab6af09343fb7db89e0532bdbd
lzianekhodja <lzianekhodja@aneo.fr>
2023-08-07 19:15:25 +0200

    Fix gcp artifact registry

--- container-registry/gcp/artifact-registry/examples/simple/versions.tf
+++ container-registry/gcp/artifact-registry/examples/simple/versions.tf
@@ -1,14 +1,26 @@
+provider "google" {
+  project = var.project
+  region  = var.region
+}
+
 terraform {
   required_version = ">= 1.0"
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 4.75.0"
+      version = "~> 4.75.0"
+    }
+    external = {
+      source  = "hashicorp/external"
+      version = "~> 2.3.1"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.2.1"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.4.0"
     }
   }
-}
-
-provider "google" {
-  project = var.project
-  region  = var.region
 }
--- container-registry/gcp/artifact-registry/main.tf
+++ container-registry/gcp/artifact-registry/main.tf
@@ -1,36 +1,36 @@
 data "google_client_config" "current" {}
 
 locals {
-labels = merge(var.labels, { module = "docker-artifact-registry" }) 
+  labels = merge(var.labels, { module = "docker-artifact-registry" })
 }
 
-resource "null_resource" "gcp_copy_images" {
-
+resource "null_resource" "copy_images" {
   for_each = var.docker_images
-
+  triggers = {
+    state = join("-", [each.key, each.value.image, each.value.tag])
+  }
   provisioner "local-exec" {
-
     command = <<-EOT
-        if [ -z "$(docker images -q '${each.value.image}:${each.value.tag}')" ]
-        then
-          if ! docker pull ${data.google_client_config.current.region}-docker.pkg.dev/${data.google_client_config.current.project}/${google_artifact_registry_repository.docker.name}/${each.value.image}:${each.value.tag}
-          then
-            echo "cannot download image ${each.value.image}:${each.value.tag}"
-            exit 1
-          fi
-        fi
-        if ! docker tag ${each.value.image} ${data.google_client_config.current.region}-docker.pkg.dev/${data.google_client_config.current.project}/${google_artifact_registry_repository.docker.name}/${each.value.image}:${each.value.tag}
-        then
-          echo "cannot tag image ${each.value.image}:${each.value.tag} to ${data.google_client_config.current.region}-docker.pkg.dev/${data.google_client_config.current.project}/${google_artifact_registry_repository.docker.name}/${each.value.image}:${each.value.tag}"
-          exit 1
-        fi
-        if ! docker push ${data.google_client_config.current.region}-docker.pkg.dev/${data.google_client_config.current.project}/${google_artifact_registry_repository.docker.name}/${each.value.image}:${each.value.tag}
-        then
-          echo "cannot push image ${data.google_client_config.current.region}-docker.pkg.dev/${data.google_client_config.current.project}/${google_artifact_registry_repository.docker.name}/${each.value.image}:${each.value.tag}"
-          exit 1
-        fi
-      
-      EOT
+gcloud auth configure-docker ${data.google_client_config.current.region}-docker.pkg.dev
+if [ -z "$(docker images -q '${each.value.image}:${each.value.tag}')" ]
+then
+  if ! docker pull ${each.value.image}:${each.value.tag}
+  then
+    echo "cannot download image ${each.value.image}:${each.value.tag}"
+    exit 1
+  fi
+fi
+if ! docker tag ${each.value.image}:${each.value.tag} ${data.google_client_config.current.region}-docker.pkg.dev/${data.google_client_config.current.project}/${google_artifact_registry_repository.docker.name}/${each.key}:${each.value.tag}
+then
+  echo "cannot tag image ${each.value.image}:${each.value.tag} to ${data.google_client_config.current.region}-docker.pkg.dev/${data.google_client_config.current.project}/${google_artifact_registry_repository.docker.name}/${each.key}:${each.value.tag}"
+  exit 1
+fi
+if ! docker push ${data.google_client_config.current.region}-docker.pkg.dev/${data.google_client_config.current.project}/${google_artifact_registry_repository.docker.name}/${each.key}:${each.value.tag}
+then
+  echo "cannot push image ${data.google_client_config.current.region}-docker.pkg.dev/${data.google_client_config.current.project}/${google_artifact_registry_repository.docker.name}/${each.key}:${each.value.tag}"
+  exit 1
+fi
+EOT
   }
 }
 
@@ -52,7 +52,7 @@ resource "google_artifact_registry_repository_iam_binding" "binding" {
   for_each   = var.iam_bindings
   project    = data.google_client_config.current.project
   location   = data.google_client_config.current.region
-  repository = var.name
+  repository = google_artifact_registry_repository.docker.name
   role       = each.key
   members    = each.value
 }

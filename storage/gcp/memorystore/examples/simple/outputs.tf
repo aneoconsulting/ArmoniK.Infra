d64e4c2d75fa95db158a2713c41fc89bd949853c
lzianekhodja <lzianekhodja@aneo.fr>
2023-08-09 18:27:48 +0200

    Update examples

--- storage/gcp/memorystore/examples/simple/outputs.tf
+++ storage/gcp/memorystore/examples/simple/outputs.tf
@@ -1,6 +1,11 @@
 output "id" {
-  description = "The memorystore instance ID."
+  description = "The Memorystore instance ID."
   value       = module.simple_memorystore.id
+}
+
+output "current_location_id" {
+  description = "The current zone where the Redis endpoint is placed."
+  value       = module.simple_memorystore.current_location_id
 }
 
 output "host" {
@@ -13,34 +18,12 @@ output "port" {
   value       = module.simple_memorystore.port
 }
 
-output "read_endpoint" {
-  description = " The IP address of the exposed readonly Redis endpoint."
-  value       = module.simple_memorystore.read_endpoint
+output "nodes" {
+  description = "Info per node"
+  value       = module.simple_memorystore.nodes
 }
 
 output "region" {
   description = "The region the instance lives in."
   value       = module.simple_memorystore.region
-}
-
-output "current_location_id" {
-  description = "The current zone where the Redis endpoint is placed."
-  value       = module.simple_memorystore.current_location_id
-}
-
-output "persistence_iam_identity" {
-  description = "Cloud IAM identity used by import/export operations. Format is 'serviceAccount:'. May change over time"
-  value       = module.simple_memorystore.persistence_iam_identity
-}
-
-output "auth_string" {
-  description = "AUTH String set on the instance. This field will only be populated if auth_enabled is true."
-  value       = module.simple_memorystore.auth_string
-  sensitive   = true
-}
-
-output "server_ca_certs" {
-  description = "List of server CA certificates for the instance"
-  value       = module.simple_memorystore.server_ca_certs
-  sensitive   = false
 }
--- storage/gcp/memorystore/examples/simple/versions.tf
+++ storage/gcp/memorystore/examples/simple/versions.tf
@@ -1,14 +1,14 @@
+provider "google" {
+  project = var.project
+  region  = var.region
+}
+
 terraform {
   required_version = ">= 1.0"
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 4.75.0"
+      version = "~> 4.75.0"
     }
   }
-}
-
-provider "google" {
-  project = var.project
-  region  = var.region
 }

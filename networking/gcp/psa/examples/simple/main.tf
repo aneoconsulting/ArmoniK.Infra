d565ac2fd74067b6db9026258a76360e8e39c54a
hbitoun-aneo <hbitoun@aneo.fr>
2023-08-16 15:54:23 +0200

    fix: add simple example + variables + versions

--- networking/gcp/psa/examples/simple/main.tf
+++ networking/gcp/psa/examples/simple/main.tf
@@ -1,26 +1,9 @@
-data "google_client_config" "current" {}
-
-data "google_compute_network" "peering_network" {
-  name    = var.vpc_network
-  project = data.google_client_config.current.project
-}
-
-resource "google_compute_global_address" "private_ip_alloc" {
-  project       = data.google_client_config.current.project
-  name          = var.global_adress_name
-  description   = var.description
-  purpose       = var.purpose
-  address       = var.address
-  prefix_length = var.prefix_length
-  ip_version    = var.ip_version
-  labels        = var.labels
-  address_type  = var.address_type
-  network       = data.google_compute_network.peering_network.id
-}
-
-# Creates the peering with the producer network.
-resource "google_service_networking_connection" "private_service_access" {
-  network                 = data.google_compute_network.peering_network.id
-  service                 = var.service_name
-  reserved_peering_ranges = [google_compute_global_address.private_ip_alloc.name]
+module "simple_example" {
+  source                    = "../../../psa"
+  vpc_network               = "my-vpc-network"
+  global_adress_name        = "my-adress"
+  global_adress_adress      = "10.132.112.84"
+  global_address_ip_version = "IPV6"
+  global_address_purpose    = "VPC_PEERING"
+  service_name              = "my-new-service"
 }

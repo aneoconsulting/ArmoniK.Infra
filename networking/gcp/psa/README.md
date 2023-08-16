4c7115add8957e8d1cfa3ed562991e3022718f7e
hbitoun-aneo <hbitoun@aneo.fr>
2023-08-16 17:18:26 +0200

    fix: add complete example + fix variable names + use of default vpc for examples

---
+++ networking/gcp/psa/README.md
@@ -0,0 +1,2 @@
+Google and third parties (together known as service producers) can offer services that are hosted in a VPC network. Private services access lets you reach the internal IP addresses of these Google and third-party services by using private connections. This is useful if you want your VM instances in your VPC network to use internal IP addresses instead of external IP addresses. The official documentation for [Private service access](https://cloud.google.com/vpc/docs/private-services-access).
+
---
+++ networking/gcp/psa/examples/complete/main.tf
@@ -0,0 +1,16 @@
+module "complete_example" {
+  source                       = "../../../psa"
+  vpc_network                  = data.google_compute_network.vpc.name
+  global_address_name          = "my-address"
+  global_address_ip            = "10.132.112.84"
+  global_address_ip_version    = "IPV6"
+  global_address_purpose       = "VPC_PEERING"
+  service_name                 = "servicenetworking.googleapis.com"
+  global_address_description   = "Description of global address for complete example"
+  global_address_prefix_length = 0
+  global_address_adress_type   = "EXTERNAL"
+}
+
+data "google_compute_network" "vpc" {
+  name = "default"
+}

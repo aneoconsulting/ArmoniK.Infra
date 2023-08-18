954327fc5be89c503d821fbd4448b11cb97055de
esoubiran-aneo <esoubiran@aneo.fr>
2023-08-18 10:40:29 +0200

    fix: update admin gui path

---
+++ armonik/admin-0-9-gui.tf
@@ -0,0 +1,217 @@
+# Control plane deployment
+resource "kubernetes_deployment" "admin_0_9_gui" {
+  count = var.admin_0_9_gui != null ? 1 : 0
+  metadata {
+    name      = "admin-0_9-gui"
+    namespace = var.namespace
+    labels = {
+      app     = "armonik"
+      service = "admin-0_9-gui"
+    }
+  }
+  spec {
+    replicas = var.admin_0_9_gui.replicas
+    selector {
+      match_labels = {
+        app     = "armonik"
+        service = "admin-0_9-gui"
+      }
+    }
+    template {
+      metadata {
+        name      = "admin-0_9-gui"
+        namespace = var.namespace
+        labels = {
+          app     = "armonik"
+          service = "admin-0_9-gui"
+        }
+      }
+      spec {
+        dynamic "toleration" {
+          for_each = (local.admin_gui_node_selector != {} ? [
+            for index in range(0, length(local.admin_gui_node_selector_keys)) : {
+              key   = local.admin_gui_node_selector_keys[index]
+              value = local.admin_gui_node_selector_values[index]
+            }
+          ] : [])
+          content {
+            key      = toleration.value.key
+            operator = "Equal"
+            value    = toleration.value.value
+            effect   = "NoSchedule"
+          }
+        }
+        dynamic "image_pull_secrets" {
+          for_each = (var.admin_0_9_gui.image_pull_secrets != "" ? [1] : [])
+          content {
+            name = var.admin_0_9_gui.image_pull_secrets
+          }
+        }
+        restart_policy = "Always" # Always, OnFailure, Never
+        # API container, only needed for 0_9 GUI
+        container {
+          name              = var.admin_0_9_gui.api.name
+          image             = var.admin_0_9_gui.api.tag != "" ? "${var.admin_0_9_gui.api.image}:${var.admin_0_9_gui.api.tag}" : var.admin_0_9_gui.api.image
+          image_pull_policy = var.admin_0_9_gui.image_pull_policy
+          resources {
+            limits   = var.admin_0_9_gui.api.limits
+            requests = var.admin_0_9_gui.api.requests
+          }
+          port {
+            name           = "api-port"
+            container_port = 3333
+          }
+          env_from {
+            config_map_ref {
+              name = kubernetes_config_map.core_config.metadata[0].name
+            }
+          }
+          env {
+            name  = "ControlPlane__Endpoint"
+            value = local.control_plane_url
+          }
+          dynamic "env" {
+            for_each = (data.kubernetes_secret.grafana.data.enabled ? [1] : [])
+            content {
+              name  = "Grafana__Endpoint"
+              value = data.kubernetes_secret.grafana.data.url
+            }
+          }
+          dynamic "env" {
+            for_each = (data.kubernetes_secret.seq.data.enabled ? [1] : [])
+            content {
+              name  = "Seq__Endpoint"
+              value = data.kubernetes_secret.seq.data.web_url
+            }
+          }
+          dynamic "env" {
+            for_each = local.credentials
+            content {
+              name = env.key
+              value_from {
+                secret_key_ref {
+                  key      = env.value.key
+                  name     = env.value.name
+                  optional = false
+                }
+              }
+            }
+          }
+          dynamic "volume_mount" {
+            for_each = local.certificates
+            content {
+              name       = volume_mount.value.name
+              mount_path = volume_mount.value.mount_path
+              read_only  = true
+            }
+          }
+        }
+        # 0_9 GUI container
+        container {
+          name              = var.admin_0_9_gui.0_9.name
+          image             = var.admin_0_9_gui.0_9.tag != "" ? "${var.admin_0_9_gui.0_9.image}:${var.admin_0_9_gui.0_9.tag}" : var.admin_0_9_gui.0_9.image
+          image_pull_policy = var.admin_0_9_gui.image_pull_policy
+          resources {
+            limits   = var.admin_0_9_gui.0_9.limits
+            requests = var.admin_0_9_gui.0_9.requests
+          }
+          port {
+            name           = "0_9-port"
+            container_port = 1080
+          }
+          env_from {
+            config_map_ref {
+              name = kubernetes_config_map.core_config.metadata[0].name
+            }
+          }
+          env {
+            name  = "ControlPlane__Endpoint"
+            value = local.control_plane_url
+          }
+          dynamic "env" {
+            for_each = (data.kubernetes_secret.grafana.data.enabled != "" ? [1] : [])
+            content {
+              name  = "Grafana__Endpoint"
+              value = data.kubernetes_secret.grafana.data.url
+            }
+          }
+          dynamic "env" {
+            for_each = (data.kubernetes_secret.seq.data.enabled ? [1] : [])
+            content {
+              name  = "Seq__Endpoint"
+              value = data.kubernetes_secret.seq.data.web_url
+            }
+          }
+          dynamic "env" {
+            for_each = local.credentials
+            content {
+              name = env.key
+              value_from {
+                secret_key_ref {
+                  key      = env.value.key
+                  name     = env.value.name
+                  optional = false
+                }
+              }
+            }
+          }
+          dynamic "volume_mount" {
+            for_each = local.certificates
+            content {
+              name       = volume_mount.value.name
+              mount_path = volume_mount.value.mount_path
+              read_only  = true
+            }
+          }
+        }
+        # Secrets volumes
+        dynamic "volume" {
+          for_each = local.certificates
+          content {
+            name = volume.value.name
+            secret {
+              secret_name = volume.value.secret_name
+              optional    = false
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+# Admin 0_9 GUI service
+resource "kubernetes_service" "admin_0_9_gui" {
+  count = length(kubernetes_deployment.admin_0_9_gui)
+  metadata {
+    name      = kubernetes_deployment.admin_0_9_gui[0].metadata[0].name
+    namespace = kubernetes_deployment.admin_0_9_gui[0].metadata[0].namespace
+    labels = {
+      app     = kubernetes_deployment.admin_0_9_gui[0].metadata[0].labels.app
+      service = kubernetes_deployment.admin_0_9_gui[0].metadata[0].labels.service
+    }
+  }
+  spec {
+    type       = var.admin_0_9_gui.service_type == "HeadLess" ? "ClusterIP" : var.admin_0_9_gui.service_type
+    cluster_ip = var.admin_0_9_gui.service_type == "HeadLess" ? "None" : null
+    selector = {
+      app     = kubernetes_deployment.admin_0_9_gui[0].metadata[0].labels.app
+      service = kubernetes_deployment.admin_0_9_gui[0].metadata[0].labels.service
+    }
+    port {
+      name        = kubernetes_deployment.admin_0_9_gui[0].spec[0].template[0].spec[0].container[0].port[0].name
+      port        = var.admin_0_9_gui.service_type == "HeadLess" ? kubernetes_deployment.admin_0_9_gui[0].spec[0].template[0].spec[0].container[0].port[0].container_port : var.admin_0_9_gui.api.port
+      target_port = kubernetes_deployment.admin_0_9_gui[0].spec[0].template[0].spec[0].container[0].port[0].container_port
+      protocol    = "TCP"
+    }
+    port {
+      name        = kubernetes_deployment.admin_0_9_gui[0].spec[0].template[0].spec[0].container[1].port[0].name
+      port        = var.admin_0_9_gui.service_type == "HeadLess" ? kubernetes_deployment.admin_0_9_gui[0].spec[0].template[0].spec[0].container[0].port[0].container_port : var.admin_0_9_gui.0_9.port
+      target_port = kubernetes_deployment.admin_0_9_gui[0].spec[0].template[0].spec[0].container[1].port[0].container_port
+      protocol    = "TCP"
+    }
+  }
+  timeouts {
+    create = "2m"
+  }
+}
--- armonik/variables.tf
+++ armonik/variables.tf
@@ -164,8 +164,50 @@ variable "admin_gui" {
   default = null
 }
 
-# Parameters of old admin gui
-variable "admin_old_gui" {
+# Deprecated, must be removed in a future version
+# Parameters of admin gui v0.8 (previously called old admin gui)
+variable "admin_0_8_gui" {
+  description = "Parameters of the old admin GUI"
+  type = object({
+    api = object({
+      name  = string
+      image = string
+      tag   = string
+      port  = number
+      limits = object({
+        cpu    = string
+        memory = string
+      })
+      requests = object({
+        cpu    = string
+        memory = string
+      })
+    })
+    old = object({
+      name  = string
+      image = string
+      tag   = string
+      port  = number
+      limits = object({
+        cpu    = string
+        memory = string
+      })
+      requests = object({
+        cpu    = string
+        memory = string
+      })
+    })
+    service_type       = string
+    replicas           = number
+    image_pull_policy  = string
+    image_pull_secrets = string
+    node_selector      = any
+  })
+  default = null
+}
+
+# Deprecated, must be removed in a future version
+variable "admin_0_9_gui" {
   description = "Parameters of the old admin GUI"
   type = object({
     api = object({

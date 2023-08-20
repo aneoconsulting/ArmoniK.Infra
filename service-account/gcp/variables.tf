3e13a4faaf8bcfa78727b6c156488bdc3f82506b
lzianekhodja <lzianekhodja@aneo.fr>
2023-08-20 12:10:36 +0200

    Update service account for pods

--- service-account/gcp/variables.tf
+++ service-account/gcp/variables.tf
@@ -1,35 +1,22 @@
-variable "gcp_sa_name" {
+variable "service_account_name" {
+  description = "Name of service account name."
   type        = string
-  description = "GCP service account name"
-  default     = null
-}
-
-variable "project_id" {
-  type        = string
-  description = "GCP project ID"
-  default     = null
 }
 
 variable "automount_service_account_token" {
+  description = "To enable automatic mounting of the Kubernetes service account token."
   type        = bool
-  description = "To enable automatic mounting of the service account token"
   default     = true
 }
 
-variable "k8s_sa_name" {
+variable "kubernetes_namespace" {
+  description = "Namespace within which name of the service account must be unique."
   type        = string
-  description = "Name of Kubernetes service account"
-  default     = null
-}
-
-variable "K8s_namespace" {
-  type        = string
-  description = "Namespace within which name of the service account must be unique"
   default     = "default"
 }
 
 variable "roles" {
-  description = "A list of roles to be added to the created service account"
+  description = "A list of roles to be added to the created service account."
   type        = set(string)
   default     = []
 }

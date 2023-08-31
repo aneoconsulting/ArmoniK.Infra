8147d54f1ee07c891a7ab98c1471e4277e9fdf07
ABLA-Adem-ANEO <aabla@aneo.fr>
2023-08-31 11:13:08 +0200

    apply pr comment

--- kubernetes/gcp/cluster/version.tf
+++ kubernetes/gcp/cluster/version.tf
@@ -1,14 +1,14 @@
 terraform {
   required_version = ">=1.0"
   required_providers {
-
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 4.75.0, < 5.0"
+    }
     null = {
       source  = "hashicorp/null"
       version = ">= 3.2.1"
     }
   }
-  provider_meta "google" {
-    module_name = "blueprints/terraform/terraform-google-kubernetes-engine/v27.0.0"
-  }
-
+ 
 }

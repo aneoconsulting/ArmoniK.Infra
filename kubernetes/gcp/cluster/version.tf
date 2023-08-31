8f314f30f8d3f74d879c4d8f55b463c8d129f7f3
ABLA-Adem-ANEO <aabla@aneo.fr>
2023-08-31 14:50:23 +0200

    apply pipe comment

--- kubernetes/gcp/cluster/version.tf
+++ kubernetes/gcp/cluster/version.tf
@@ -1,10 +1,7 @@
 terraform {
   required_version = ">=1.0"
   required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = ">= 4.75.0, < 5.0"
-    }
+    
     null = {
       source  = "hashicorp/null"
       version = ">= 3.2.1"

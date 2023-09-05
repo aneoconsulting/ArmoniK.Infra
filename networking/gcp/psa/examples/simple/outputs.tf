eda878534c337423fe4211f14974342dd2590fff
hbitoun-aneo <hbitoun@aneo.fr>
2023-09-05 09:17:24 +0200

    fix: CI changes

--- networking/gcp/psa/examples/simple/outputs.tf
+++ networking/gcp/psa/examples/simple/outputs.tf
@@ -1,9 +1,9 @@
 output "private_ip_alloc" {
-    description = "The IP address reserved for the VPC"
-    value       = module.simple_example.private_ip_alloc
+  description = "The IP address reserved for the VPC"
+  value       = module.simple_example.private_ip_alloc
 }
 
 output "private_service_access" {
-    description = "The PSA"
-    value       = module.simple_example.private_service_access
+  description = "The PSA"
+  value       = module.simple_example.private_service_access
 }

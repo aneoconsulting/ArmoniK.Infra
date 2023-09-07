a2df6e9fd83bd1e55705f86ceb667da4897b0066
hbitoun-aneo <hbitoun@aneo.fr>
2023-09-07 10:06:10 +0200

    fix: changes from CI

--- security/gcp/get-kms/examples/simple/main.tf
+++ security/gcp/get-kms/examples/simple/main.tf
@@ -1,5 +1,5 @@
 module "simple_kms" {
   source           = "../../../get-kms"
   key_ring_name    = "test"
-  crypto_key_names = ["my-key-name","my-key-name2"]
+  crypto_key_names = ["my-key-name", "my-key-name2"]
 }

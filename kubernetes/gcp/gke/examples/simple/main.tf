e030e07af2c48950f9ade32bb03c8312c043c8c0
lzianekhodja <lzianekhodja@aneo.fr>
2023-09-08 22:40:33 +0200

    manage master_authorized_networks in private GKE

--- kubernetes/gcp/gke/examples/simple/main.tf
+++ kubernetes/gcp/gke/examples/simple/main.tf
@@ -1,6 +1,6 @@
 module "vpc" {
-  source = "../../../../../networking/gcp/vpc"
-  name   = "gke-simple-test"
+  source     = "../../../../../networking/gcp/vpc"
+  name       = "gke-simple-test"
   gke_subnet = {
     name                = "gke-simple-test"
     nodes_cidr_block    = "10.43.0.0/16",
@@ -11,12 +11,15 @@ module "vpc" {
 }
 
 module "gke" {
-  source            = "../../../gke"
-  name              = "gke-simple-test"
-  network           = module.vpc.name
-  ip_range_pods     = module.vpc.gke_subnet_pods_range_name
-  ip_range_services = module.vpc.gke_subnet_svc_range_name
-  subnetwork        = module.vpc.gke_subnet_name
-  kubeconfig_path   = abspath("${path.root}/generated/kubeconfig")
-  private = false
+  source                     = "../../../gke"
+  name                       = "gke-simple-test"
+  network                    = module.vpc.name
+  ip_range_pods              = module.vpc.gke_subnet_pods_range_name
+  ip_range_services          = module.vpc.gke_subnet_svc_range_name
+  subnetwork                 = module.vpc.gke_subnet_name
+  subnetwork_cidr            = module.vpc.gke_subnet_cidr_block
+  kubeconfig_path            = abspath("${path.root}/generated/kubeconfig")
+  # GKE is private by default, add 0.0.0.0/0 to master_authorized_networks
+  # to access GKE from outside of the GCP VPC.
+  master_authorized_networks = [{ cidr_block = "0.0.0.0/0", display_name = "EXTERNAL" }]
 }
--- kubernetes/gcp/gke/main.tf
+++ kubernetes/gcp/gke/main.tf
@@ -1,11 +1,5 @@
 data "google_client_config" "current" {}
 
-data "google_compute_subnetwork" "subnetwork" {
-  name    = var.subnetwork
-  project = data.google_client_config.current.project
-  region  = can(coalesce(var.region)) ? var.region : data.google_client_config.current.region
-}
-
 data "google_compute_zones" "available" {
   project = data.google_client_config.current.project
   region  = can(coalesce(var.region)) ? var.region : data.google_client_config.current.region
@@ -15,60 +9,66 @@ data "google_compute_zones" "available" {
 locals {
   description                     = coalesce(var.description, "GKE of name ${var.name}")
   enable_vertical_pod_autoscaling = !var.horizontal_pod_autoscaling ? var.enable_vertical_pod_autoscaling : false
-  master_authorized_networks = var.private ? [{
-    cidr_block   = data.google_compute_subnetwork.subnetwork.ip_cidr_range
-    display_name = "VPC"
-  }] : var.master_authorized_networks
-  node_pools = [for node_pool in var.node_pools : merge(node_pool, { name = "${var.name}-${node_pool["name"]}" })]
-  region     = var.regional && !can(coalesce(var.region)) ? data.google_client_config.current.region : var.region
-  zones      = !var.regional && length(var.zones) == 0 ? data.google_compute_zones.available.names : var.zones
+  master_authorized_networks      = distinct(concat(var.master_authorized_networks, var.private ? [
+    {
+      cidr_block   = var.subnetwork_cidr
+      display_name = "CLUSTER-VPC"
+    }
+  ] : []))
+  node_pools = [
+  for node_pool in var.node_pools : merge(node_pool, {
+    name = "${var.name}-${node_pool["name"]}"
+  })
+  ]
+  region = var.regional && !can(coalesce(var.region)) ? data.google_client_config.current.region : var.region
+  zones  = !var.regional && length(var.zones) == 0 ? data.google_compute_zones.available.names : var.zones
 }
 
 module "gke_standard" {
-  source      = "terraform-google-modules/kubernetes-engine/google"
-  version     = "27.0.0"
-  description = local.description
+  source                                = "terraform-google-modules/kubernetes-engine/google"
+  version                               = "27.0.0"
+  description                           = local.description
   # Required
-  ip_range_pods     = var.ip_range_pods
-  ip_range_services = var.ip_range_services
-  name              = var.name
-  network           = var.network
-  project_id        = data.google_client_config.current.project
-  subnetwork        = var.subnetwork
+  ip_range_pods                         = var.ip_range_pods
+  ip_range_services                     = var.ip_range_services
+  name                                  = var.name
+  network                               = var.network
+  project_id                            = data.google_client_config.current.project
+  subnetwork                            = var.subnetwork
   # Optional
-  cluster_autoscaling                = var.cluster_autoscaling
-  cluster_resource_labels            = var.cluster_resource_labels
-  create_service_account             = var.create_service_account
-  database_encryption                = var.database_encryption
-  default_max_pods_per_node          = var.default_max_pods_per_node
-  enable_cost_allocation             = var.enable_cost_allocation
-  enable_resource_consumption_export = var.enable_resource_consumption_export
-  enable_shielded_nodes              = var.enable_shielded_nodes
-  enable_vertical_pod_autoscaling    = local.enable_vertical_pod_autoscaling
-  filestore_csi_driver               = var.filestore_csi_driver
-  gce_pd_csi_driver                  = var.gce_pd_csi_driver
-  gke_backup_agent_config            = var.gke_backup_agent_config
-  grant_registry_access              = var.grant_registry_access
-  horizontal_pod_autoscaling         = var.horizontal_pod_autoscaling
-  initial_node_count                 = var.initial_node_count
-  kubernetes_version                 = var.kubernetes_version
-  logging_enabled_components         = var.logging_enabled_components
-  monitoring_enabled_components      = var.monitoring_enabled_components
-  network_policy                     = var.network_policy
-  network_policy_provider            = var.network_policy_provider
-  node_metadata                      = var.node_metadata
-  node_pools                         = local.node_pools
-  node_pools_labels                  = var.node_pools_labels
-  node_pools_resource_labels         = var.node_pools_resource_labels
-  node_pools_tags                    = var.node_pools_tags
-  node_pools_taints                  = var.node_pools_taints
-  region                             = local.region
-  regional                           = var.regional
-  remove_default_node_pool           = var.remove_default_node_pool
-  service_account                    = var.service_account
-  service_account_name               = var.service_account_name
-  windows_node_pools                 = var.windows_node_pools
-  zones                              = local.zones
+  cluster_autoscaling                   = var.cluster_autoscaling
+  cluster_resource_labels               = var.cluster_resource_labels
+  create_service_account                = var.create_service_account
+  database_encryption                   = var.database_encryption
+  default_max_pods_per_node             = var.default_max_pods_per_node
+  enable_cost_allocation                = var.enable_cost_allocation
+  enable_resource_consumption_export    = var.enable_resource_consumption_export
+  enable_shielded_nodes                 = var.enable_shielded_nodes
+  enable_vertical_pod_autoscaling       = local.enable_vertical_pod_autoscaling
+  filestore_csi_driver                  = var.filestore_csi_driver
+  gce_pd_csi_driver                     = var.gce_pd_csi_driver
+  gke_backup_agent_config               = var.gke_backup_agent_config
+  grant_registry_access                 = var.grant_registry_access
+  horizontal_pod_autoscaling            = var.horizontal_pod_autoscaling
+  initial_node_count                    = var.initial_node_count
+  kubernetes_version                    = var.kubernetes_version
+  logging_enabled_components            = var.logging_enabled_components
+  monitoring_enabled_components         = var.monitoring_enabled_components
+  network_policy                        = var.network_policy
+  network_policy_provider               = var.network_policy_provider
+  node_metadata                         = var.node_metadata
+  node_pools                            = local.node_pools
+  node_pools_labels                     = var.node_pools_labels
+  node_pools_resource_labels            = var.node_pools_resource_labels
+  node_pools_tags                       = var.node_pools_tags
+  node_pools_taints                     = var.node_pools_taints
+  region                                = local.region
+  regional                              = var.regional
+  remove_default_node_pool              = var.remove_default_node_pool
+  service_account                       = var.service_account
+  service_account_name                  = var.service_account_name
+  windows_node_pools                    = var.windows_node_pools
+  zones                                 = local.zones
   # Optional and not used yet
   add_cluster_firewall_rules            = var.add_cluster_firewall_rules
   add_master_webhook_firewall_rules     = var.add_master_webhook_firewall_rules
@@ -122,7 +122,7 @@ module "gke_standard" {
 resource "null_resource" "update_kubeconfig" {
   count = can(coalesce(var.kubeconfig_path)) ? 1 : 0
   provisioner "local-exec" {
-    command = "gcloud container clusters get-credentials ${module.gke_standard.name} --location=${module.gke_standard.location}"
+    command     = "gcloud container clusters get-credentials ${module.gke_standard.name} --location=${module.gke_standard.location}"
     environment = {
       KUBECONFIG = abspath(var.kubeconfig_path)
     }

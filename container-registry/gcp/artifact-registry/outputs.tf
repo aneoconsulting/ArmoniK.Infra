8ede0a2f932553f62b4c9712691cf64146abe497
lzianekhodja <lzianekhodja@aneo.fr>
2023-09-12 16:19:57 +0200

    update output of artifact registry

--- container-registry/gcp/artifact-registry/outputs.tf
+++ container-registry/gcp/artifact-registry/outputs.tf
@@ -1,6 +1,6 @@
 output "docker_repositories" {
   description = "Docker repositories in Artifactory Registry created on GCP"
-  value       = { for image, _ in null_resource.copy_images : image => "${data.google_client_config.current.region}-docker.pkg.dev/${data.google_client_config.current.project}/${google_artifact_registry_repository.docker.name}/${image}" }
+  value       = { for key, value in null_resource.copy_images : "${value.image}:${value.tag}" => "${data.google_client_config.current.region}-docker.pkg.dev/${data.google_client_config.current.project}/${google_artifact_registry_repository.docker.name}/${value.name}:${value.tag}" }
 }
 
 output "kms_key_name" {

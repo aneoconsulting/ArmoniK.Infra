b6b0e396dcb36585c6906893b029c15a18065adf
lzianekhodja <lzianekhodja@aneo.fr>
2023-09-13 10:23:35 +0200

    Add extraconf for jobs connectinf to the state database

--- monitoring/onpremise/exporters/partition-metrics-exporter/configmap.tf
+++ monitoring/onpremise/exporters/partition-metrics-exporter/configmap.tf
@@ -4,15 +4,14 @@ resource "kubernetes_config_map" "partition_metrics_exporter_config" {
     name      = "partition-metrics-exporter-configmap"
     namespace = var.namespace
   }
-  data = merge(var.extra_conf, {
+  data = merge({
     MongoDB__CAFile           = local.secrets.mongodb.ca_filename
     MongoDB__ReplicaSet       = "rs0"
     MongoDB__DatabaseName     = "database"
-    MongoDB__DataRetention    = "10.00:00:00"
     MongoDB__DirectConnection = "false"
     MongoDB__Tls              = "true"
     MetricsExporter__Host     = "http://${local.metrics_exporter_host}"
     MetricsExporter__Port     = local.metrics_exporter_port
     MetricsExporter__Path     = "/metrics"
-  })
+  }, var.extra_conf)
 }

fc91856ea3fed4969f056e8884f83f1b88490763
YKharouni <ykharouni@aneo.fr>
2023-09-27 10:48:10 +0200

    add simple example

--- kubernetes/aws/eks/examples/simple/main.tf
+++ kubernetes/aws/eks/examples/simple/main.tf
@@ -30,23 +30,23 @@ data "aws_subnets" "subnets" {
 # AWS EKS
 module "eks" {
   source                                                   = "../.."
-  name                                                     = "eks-complete-test"
+  name                                                     = "eks-simple-test"
   profile                                                  = "default"
-  cluster_autoscaler_expander                              = "least-waste"
+  #cluster_autoscaler_expander                              = "least-waste"
   cluster_autoscaler_image                                 = "registry.k8s.io/autoscaling/cluster-autoscaler"
-  cluster_autoscaler_max_node_provision_time               = "15m0s"
-  cluster_autoscaler_min_replica_count                     = 0
+  #cluster_autoscaler_max_node_provision_time               = "15m0s"
+  #cluster_autoscaler_min_replica_count                     = 0
   cluster_autoscaler_namespace                             = "kube-system"
   cluster_autoscaler_repository                            = "https://kubernetes.github.io/autoscaler"
-  cluster_autoscaler_scale_down_delay_after_add            = "2m"
+  #cluster_autoscaler_scale_down_delay_after_add            = "2m"
   cluster_autoscaler_scale_down_delay_after_delete         = "0s"
-  cluster_autoscaler_scale_down_delay_after_failure        = "3m"
-  cluster_autoscaler_scale_down_enabled                    = true
-  cluster_autoscaler_scale_down_non_empty_candidates_count = 30
-  cluster_autoscaler_scale_down_unneeded_time              = "2m"
-  cluster_autoscaler_scale_down_utilization_threshold      = 0.5
-  cluster_autoscaler_scan_interval                         = "10s"
-  cluster_autoscaler_skip_nodes_with_system_pods           = true
+  #cluster_autoscaler_scale_down_delay_after_failure        = "3m"
+  #cluster_autoscaler_scale_down_enabled                    = true
+  #cluster_autoscaler_scale_down_non_empty_candidates_count = 30
+  #cluster_autoscaler_scale_down_unneeded_time              = "2m"
+  #cluster_autoscaler_scale_down_utilization_threshold      = 0.5
+  #cluster_autoscaler_scan_interval                         = "10s"
+  #cluster_autoscaler_skip_nodes_with_system_pods           = true
   cluster_autoscaler_tag                                   = "v1.28.0"
   cluster_autoscaler_version                               = "9.29.3"
   cluster_encryption_config                                = ""
@@ -68,38 +68,11 @@ module "eks" {
   vpc_id                                                   = data.aws_vpc.default.id
   vpc_pods_subnet_ids                                      = data.aws_subnets.subnets.ids
   vpc_private_subnet_ids                                   = data.aws_subnets.subnets.ids
-  eks_managed_node_groups = {
-    test = {
-      name                        = "workers"
-      launch_template_description = "Node group for test"
-      ami_type                    = "AL2_x86_64"
-      instance_types              = ["c5.large"]
-      capacity_type               = "SPOT"
-      min_size                    = 0
-      desired_size                = 1
-      max_size                    = 1000
-      labels = {
-        service                        = "workers"
-        "node.kubernetes.io/lifecycle" = "spot"
-      }
-      taints = {
-        dedicated = {
-          key    = "service"
-          value  = "workers"
-          effect = "NO_SCHEDULE"
-        }
-      }
-      iam_role_use_name_prefix = false
-      iam_role_additional_policies = {
-        AmazonSSMManagedInstanceCore = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-      }
-    }
-  }
 
   self_managed_node_groups = {
-    others = {
-      name                        = "others"
-      launch_template_description = "Node group for others"
+    workers = {
+      name                        = "workers"
+      launch_template_description = "Node group for workers"
       instance_type               = "c5.large"
       min_size                    = 0
       desired_size                = 1
@@ -110,36 +83,6 @@ module "eks" {
         market_type = "spot"
       }
       bootstrap_extra_args     = "--kubelet-extra-args '--node-labels=node.kubernetes.io/lifecycle=spot'"
-      iam_role_use_name_prefix = false
-      iam_role_additional_policies = {
-        AmazonSSMManagedInstanceCore = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-      }
-    }
-    others_mixed = {
-      name                        = "others-mixed"
-      launch_template_description = "Mixed On demand and SPOT instances for other pods"
-      min_size                    = 0
-      desired_size                = 1
-      max_size                    = 5
-      use_mixed_instances_policy  = true
-      mixed_instances_policy = {
-        on_demand_allocation_strategy            = "lowest-price"
-        on_demand_base_capacity                  = 0
-        on_demand_percentage_above_base_capacity = 20 # 20% On-Demand Instances, 80% Spot Instances
-        spot_allocation_strategy                 = "price-capacity-optimized"
-        spot_instance_pools                      = null
-        spot_max_price                           = null
-      }
-      override = [
-        {
-          instance_type     = "c5.4xlarge"
-          weighted_capacity = "1"
-        },
-        {
-          instance_type     = "c5.2xlarge"
-          weighted_capacity = "2"
-        },
-      ]
       iam_role_use_name_prefix = false
       iam_role_additional_policies = {
         AmazonSSMManagedInstanceCore = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
---
+++ kubernetes/aws/eks/examples/simple/providers.tf
@@ -0,0 +1,18 @@
+
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  config_path            = "${path.root}/generated/kubeconfig"
+}
+
+# package manager for kubernetes
+provider "helm" {
+  helm_driver = "configmap"
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    #token                  = module.eks.token
+    insecure    = false
+    config_path = "${path.root}/generated/kubeconfig"
+  }
+}

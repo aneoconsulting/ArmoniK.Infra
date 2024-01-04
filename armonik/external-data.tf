cfea276f8a470b1d8307ff8e403ca6c24421b512
alouvion-aneo <113617172+alouvion-aneo@users.noreply.github.com>
2024-01-04 17:16:55 +0100

    remove_old_admin_gui

--- armonik/external-data.tf
+++ armonik/external-data.tf
@@ -18,20 +18,6 @@ module "admin_gui_endpoint" {
   cluster_domain = local.cluster_domain
 }
 
-# Deprecated, must be removed in a future version
-module "admin_0_8_gui_endpoint" {
-  source         = "../utils/service-ip"
-  service        = one(kubernetes_service.admin_0_8_gui)
-  cluster_domain = local.cluster_domain
-}
-
-# Deprecated, must be removed in a future version
-module "admin_0_9_gui_endpoint" {
-  source         = "../utils/service-ip"
-  service        = one(kubernetes_service.admin_0_9_gui)
-  cluster_domain = local.cluster_domain
-}
-
 module "ingress_endpoint" {
   source         = "../utils/service-ip"
   service        = one(kubernetes_service.ingress)
@@ -48,17 +34,6 @@ locals {
     ip       = module.admin_gui_endpoint.host
     app_port = try(module.admin_gui_endpoint.ports[0], null)
   }
-  # Deprecated, must be removed in a future version
-  admin_0_8_gui_endpoints = {
-    ip       = module.admin_0_8_gui_endpoint.host
-    api_port = try(module.admin_0_8_gui_endpoint.ports[0], null)
-    app_port = try(module.admin_0_8_gui_endpoint.ports[1], null)
-  }
-  # Deprecated, must be removed in a future version
-  admin_0_9_gui_endpoints = {
-    ip   = module.admin_0_9_gui_endpoint.host
-    port = try(module.admin_0_9_gui_endpoint.ports[0], null)
-  }
   ingress_endpoint = {
     ip        = module.ingress_endpoint.host
     http_port = var.ingress.http_port
@@ -66,12 +41,6 @@ locals {
   }
   control_plane_url = "http://${local.control_plane_endpoints.ip}:${local.control_plane_endpoints.port}"
   admin_app_url     = length(kubernetes_service.admin_gui) > 0 ? "http://${local.admin_gui_endpoints.ip}:${local.admin_gui_endpoints.app_port}" : null
-  # Deprecated, must be removed in a future version
-  admin_api_url = length(kubernetes_service.admin_0_8_gui) > 0 ? "http://${local.admin_0_8_gui_endpoints.ip}:${local.admin_0_8_gui_endpoints.api_port}/api" : null
-  # Deprecated, must be removed in a future version
-  admin_0_8_url = length(kubernetes_service.admin_0_8_gui) > 0 ? "http://${local.admin_0_8_gui_endpoints.ip}:${local.admin_0_8_gui_endpoints.app_port}/" : null
-  # Deprecated, must be removed in a future version
-  admin_0_9_url    = length(kubernetes_service.admin_0_9_gui) > 0 ? "http://${local.admin_0_9_gui_endpoints.ip}:${local.admin_0_9_gui_endpoints.port}/" : null
-  ingress_http_url = var.ingress != null ? "${var.ingress.tls ? "https" : "http"}://${local.ingress_endpoint.ip}:${local.ingress_endpoint.http_port}" : ""
-  ingress_grpc_url = var.ingress != null ? "${var.ingress.tls ? "https" : "http"}://${local.ingress_endpoint.ip}:${local.ingress_endpoint.grpc_port}" : ""
+  ingress_http_url  = var.ingress != null ? "${var.ingress.tls ? "https" : "http"}://${local.ingress_endpoint.ip}:${local.ingress_endpoint.http_port}" : ""
+  ingress_grpc_url  = var.ingress != null ? "${var.ingress.tls ? "https" : "http"}://${local.ingress_endpoint.ip}:${local.ingress_endpoint.grpc_port}" : ""
 }

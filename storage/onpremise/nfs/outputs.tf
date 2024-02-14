9e0488ad07cc418c71a08bfd4876e7f4fa9673b0
Junior Dongo <jdongo@aneo.fr>
2024-02-14 11:40:24 +0100

    add description for nfs module outputs

--- storage/onpremise/nfs/outputs.tf
+++ storage/onpremise/nfs/outputs.tf
@@ -1,10 +1,12 @@
 # nfs
 
 output "provisioner_name" {
-  value = kubernetes_deployment.nfs_provisioner.metadata[0].name
+  description = "name of the created provisionner"
+  value       = kubernetes_deployment.nfs_provisioner.metadata[0].name
 }
 
 
 output "pvc_name" {
-  value = kubernetes_persistent_volume_claim.nfs_claim.metadata[0].name
+  description = "name of the created persistant volume claim"
+  value       = kubernetes_persistent_volume_claim.nfs_claim.metadata[0].name
 }

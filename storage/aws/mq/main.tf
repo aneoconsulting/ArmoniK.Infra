84b24004503e2bf58d3d9342da7f4e44fade9b57
lzianekhodja <lzianekhodja@aneo.fr>
2024-02-19 10:58:18 +0100

    update mq

--- storage/aws/mq/main.tf
+++ storage/aws/mq/main.tf
@@ -27,23 +27,22 @@ resource "random_password" "password" {
 }
 
 resource "aws_mq_broker" "mq" {
-  broker_name             = var.name
-  engine_type             = var.engine_type
-  engine_version          = var.engine_version
-  host_instance_type      = var.host_instance_type
-  apply_immediately       = var.apply_immediately
-  deployment_mode         = (var.engine_type == "RabbitMQ" && var.deployment_mode == "ACTIVE_STANDBY_MULTI_AZ") ? "CLUSTER_MULTI_AZ" : (var.engine_type == "ActiveMQ" && var.deployment_mode == "CLUSTER_MULTI_AZ") ? "ACTIVE_STANDBY_MULTI_AZ" : var.deployment_mode
-  storage_type            = var.engine_type == "RabbitMQ" ? "ebs" : var.deployment_mode == "ACTIVE_STANDBY_MULTI_AZ" ? "efs" : var.storage_type # only ebs is supported for RabbitMQ
-  authentication_strategy = var.engine_type == "RabbitMQ" ? "simple" : var.authentication_strategy                                              # ldap is not supported for RabbitMQ
-  publicly_accessible     = var.publicly_accessible
-  security_groups         = [aws_security_group.mq.id]
-  subnet_ids              = local.subnet_ids
-  dynamic "configuration" {
-    for_each = var.engine_type == "ActiveMQ" ? [1] : []
-    content {
-      id       = one(aws_mq_configuration.mq_configuration[*].id)
-      revision = one(aws_mq_configuration.mq_configuration[*].latest_revision)
-    }
+  broker_name        = var.name
+  engine_type        = var.engine_type
+  engine_version     = var.engine_version
+  host_instance_type = var.host_instance_type
+  apply_immediately  = var.apply_immediately
+  deployment_mode    = (var.engine_type == "RabbitMQ" && var.deployment_mode == "ACTIVE_STANDBY_MULTI_AZ") ? "CLUSTER_MULTI_AZ" : (var.engine_type == "ActiveMQ" && var.deployment_mode == "CLUSTER_MULTI_AZ") ? "ACTIVE_STANDBY_MULTI_AZ" : var.deployment_mode
+  storage_type       = var.engine_type == "RabbitMQ" ? "ebs" : var.deployment_mode == "ACTIVE_STANDBY_MULTI_AZ" ? "efs" : var.storage_type
+  # only ebs is supported for RabbitMQ
+  authentication_strategy = var.engine_type == "RabbitMQ" ? "simple" : var.authentication_strategy
+  # ldap is not supported for RabbitMQ
+  publicly_accessible = var.publicly_accessible
+  security_groups     = var.engine_type == "ActiveMQ" ? [aws_security_group.mq.id] : null
+  subnet_ids          = local.subnet_ids
+  configuration {
+    id       = var.engine_type == "ActiveMQ" ? aws_mq_configuration.activemq_configuration[0].id : aws_mq_configuration.rabbitmq_configuration[0].id
+    revision = var.engine_type == "ActiveMQ" ? aws_mq_configuration.activemq_configuration[0].latest_revision : aws_mq_configuration.rabbitmq_configuration[0].latest_revision
   }
   encryption_options {
     kms_key_id        = var.kms_key_id
@@ -63,7 +62,7 @@ resource "aws_mq_broker" "mq" {
 }
 
 # MQ configuration only if engine type is ActiveMQ
-resource "aws_mq_configuration" "mq_configuration" {
+resource "aws_mq_configuration" "activemq_configuration" {
   count          = var.engine_type == "ActiveMQ" ? 1 : 0
   description    = "ArmoniK ActiveMQ Configuration"
   name           = var.name
@@ -117,10 +116,51 @@ DATA
   tags           = local.tags
 }
 
+# MQ configuration only if engine type is RabbitMQ
+resource "aws_mq_configuration" "rabbitmq_configuration" {
+  count          = var.engine_type == "RabbitMQ" ? 1 : 0
+  description    = "ArmoniK RabbitMQ Configuration"
+  name           = var.name
+  engine_type    = var.engine_type
+  engine_version = var.engine_version
+  data           = <<DATA
+## DEFAULT SETTINGS ARE NOT MEANT TO BE TAKEN STRAIGHT INTO PRODUCTION
+## see https://www.rabbitmq.com/configure.html for further information
+## on configuring RabbitMQ
+# https://github.com/rabbitmq/rabbitmq-server/blob/main/deps/rabbit/docs/rabbitmq.conf.example
+# https://github.com/rabbitmq/rabbitmq-server/blob/main/deps/rabbit/docs/advanced.config.example
+# Default RabbitMQ delivery acknowledgement timeout is 30 minutes
+consumer_timeout = 1800000
+
+## allow access to the guest user from anywhere on the network
+## https://www.rabbitmq.com/access-control.html#loopback-users
+## https://www.rabbitmq.com/production-checklist.html#users
+loopback_users.guest = false
+
+## Send all logs to stdout/TTY. Necessary to see logs when running via
+## a container
+log.console = true
+
+## Defines the threshold of RAM using at which publishers to the queue are throttled
+## https://www.rabbitmq.com/memory.html
+vm_memory_high_watermark.relative = 0.75
+
+## Defines the threshold of memory using at which publishers to the queue are throttled
+## https://www.rabbitmq.com/disk-alarms.html
+disk_free_limit.relative = 0.75
+
+## TLS listeners are configured in the same fashion as TCP listeners,
+## including the option to control the choice of interface.
+##
+# listeners.ssl.default = 5671
+DATA
+  tags           = local.tags
+}
+
 # MQ security group
 resource "aws_security_group" "mq" {
   name        = "${var.name}-sg"
-  description = "Allow Amazon MQ inbound traffic on port 5672"
+  description = "Allow Amazon MQ inbound traffic on port 5671"
   vpc_id      = var.vpc_id
   ingress {
     description = "tcp from Amazon MQ"
@@ -129,12 +169,35 @@ resource "aws_security_group" "mq" {
     protocol    = "tcp"
     cidr_blocks = var.vpc_cidr_blocks
   }
-  ingress {
-    description = "Web console for Amazon MQ"
-    from_port   = 8162
-    to_port     = 8162
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+  dynamic "ingress" {
+    for_each = var.publicly_accessible && var.engine_type == "ActiveMQ" ? [1] : []
+    content {
+      description = "Web console for Amazon MQ broker ActiveMQ"
+      from_port   = 8162
+      to_port     = 8162
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+  dynamic "ingress" {
+    for_each = var.publicly_accessible && var.engine_type == "RabbitMQ" ? [1] : []
+    content {
+      description = "Web console for Amazon MQ broker RabbitMQ"
+      from_port   = 443
+      to_port     = 443
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+  dynamic "ingress" {
+    for_each = var.publicly_accessible && var.engine_type == "RabbitMQ" ? [1] : []
+    content {
+      description = "Web console for Amazon MQ broker RabbitMQ"
+      from_port   = 15672
+      to_port     = 15672
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
   }
   egress {
     from_port   = 0
@@ -142,6 +205,17 @@ resource "aws_security_group" "mq" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  /*dynamic "ingress" {
+    for_each = var.publicly_accessible ? [1] : []
+    content {
+      description = "OpenWire for Amazon MQ"
+      from_port   = 61616
+      to_port     = 61616
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+  */
   tags = local.tags
 }
 

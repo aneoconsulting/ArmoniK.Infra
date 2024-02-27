18ef8895f08139ba681a16645bdd760197c176d3
lzianekhodja <lzianekhodja@aneo.fr>
2024-02-28 00:06:52 +0100

    role binding cluster autoscaler

--- kubernetes/aws/eks/autoscaling-group-tag.tf
+++ kubernetes/aws/eks/autoscaling-group-tag.tf
@@ -47,5 +47,17 @@ output "test" {
         value = tag_value
       }
     }
-  ]...) : module.eks.self_managed_node_groups[v.mng].autoscaling_group_name]
+  ]...) : distinct(module.eks.self_managed_node_groups[v.mng].autoscaling_group_name)]
+}
+
+output "test_2" {
+  value = [ for k,v in merge([
+    for self_mng, tags in local.eks_autoscaling_group_tags : {
+      for tag_key, tag_value in tags : "${self_mng}-${substr(tag_key, 25, -1)}" => {
+        mng   = self_mng,
+        key   = tag_key,
+        value = tag_value
+      }
+    }
+  ]...) : distinct(module.eks.eks_managed_node_groups[v.mng].node_group_autoscaling_group_names)]
 }

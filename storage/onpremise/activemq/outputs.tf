94962ba0cd9adba803c438de9c8c026f1ba23ac6
Junior Dongo <jdongo@aneo.fr>
2024-04-29 15:35:42 +0200

    activemq outputs

--- storage/onpremise/activemq/outputs.tf
+++ storage/onpremise/activemq/outputs.tf
@@ -57,3 +57,42 @@ output "adapter_absolute_path" {
   description = "Absolute path for the queue adapter"
   value       = local.adapter_absolute_path
 }
+
+#new Outputs 
+
+output "env" {
+  description = "Elements to be set as environment variables"
+  value = (merge({
+    "Components__QueueAdaptorSettings__ClassName"           = local.adapter_class_name
+    "Components__QueueAdaptorSettings__AdapterAbsolutePath" = local.adapter_absolute_path
+    "Amqp__Host"                                            = local.activemq_endpoints.ip
+    "Amqp__Port"                                            = local.activemq_endpoints.port
+    "Amqp__Scheme"                                          = var.scheme
+    "Amqp__CaPath"                                          = "${var.path}/chain.pem"
+  }, var.extra_conf))
+
+}
+
+output "env_from_secret" {
+  description = "Secrets to be set as environment variables"
+  value = {
+    secret     = kubernetes_secret.activemq_user_credentials.metadata[0].name
+    credential = kubernetes_secret.activemq_user.metadata[0].name
+  }
+}
+
+output "mount_secret" {
+  description = "Secrets to be mounted as volumes"
+  value = {
+    "secret1" = {
+      secret = kubernetes_secret.activemq
+      path   = var.path
+      mode   = "0600"
+    },
+    "secret2" = {
+      secret = kubernetes_secret.activemq_user
+      path   = "/cred"
+      mode   = "0600"
+    }
+  }
+}

edea3fe859fad0cfb168d2225389aa040a391d73
Junior Dongo <jdongo@aneo.fr>
2024-05-06 08:45:12 +0200

    fix: nfs output

--- storage/onpremise/nfs/outputs.tf
+++ storage/onpremise/nfs/outputs.tf
@@ -10,3 +10,22 @@ output "pvc_name" {
   description = "name of the created persistant volume claim"
   value       = kubernetes_persistent_volume_claim.nfs_claim.metadata[0].name
 }
+
+#new outputs
+output "env" {
+  description = "Elements to be set as environment variables"
+  value = ({
+    "Components__ObjectStorage" = var.object_storage_adapter
+    "LocalStorage__Path"        = var.mount_path
+  })
+}
+
+output "mount_volume" {
+  description = "Volume to be mounted"
+  value = {
+    "volume1" = {
+      path       = var.mount_path
+      claim_name = var.pvc_name
+    }
+  }
+}

754417552da2c80c35ace49844a82a6edc11ba7a
tschneider-aneo <tschneider@aneo.fr>
2024-05-17 11:07:07 +0200

    Remove cluster domain in mongodb dns

--- storage/onpremise/mongodb/locals.tf
+++ storage/onpremise/mongodb/locals.tf
@@ -1,6 +1,6 @@
 locals {
   node_selector_keys   = keys(var.mongodb.node_selector)
   node_selector_values = values(var.mongodb.node_selector)
-  mongodb_dns          = "${var.helm_release_name}-headless.${var.namespace}.svc.cluster.local"
+  mongodb_dns          = "${var.helm_release_name}-headless.${var.namespace}.svc"
   mongodb_url          = "mongodb+srv://${local.mongodb_dns}/${jsondecode(helm_release.mongodb.metadata[0].values).auth.databases[0]}"
 }

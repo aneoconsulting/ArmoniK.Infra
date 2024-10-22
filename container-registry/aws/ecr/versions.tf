4ef3436ff4add10a425c3c242fea17405e3ef717
Junior Dongo <jdongo@aneo.fr>
2024-10-22 16:33:59 +0200

    fix: replace docker pull push with scopeo

--- container-registry/aws/ecr/versions.tf
+++ container-registry/aws/ecr/versions.tf
@@ -1,3 +1,10 @@
+provider "skopeo2" {
+  destination {
+    login_username = data.aws_ecr_authorization_token.current.user_name
+    login_password = data.aws_ecr_authorization_token.current.password
+  }
+}
+
 terraform {
   required_version = ">= 1.0"
   required_providers {
@@ -5,9 +12,9 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 5.4.0"
     }
-    null = {
-      source  = "hashicorp/null"
-      version = ">= 3.2.1"
+    skopeo2 = {
+      source  = "bsquare-corp/skopeo2"
+      version = "~> 1.1.1"
     }
   }
 }

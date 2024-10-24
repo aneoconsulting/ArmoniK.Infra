a7b6ea1a2470e7a2266ec5045dbf170e2630f6bf
Junior Dongo <jdongo@aneo.fr>
2024-10-24 12:14:13 +0200

    fix: mongo dns

--- storage/onpremise/mongodb/locals.tf
+++ storage/onpremise/mongodb/locals.tf
@@ -1,4 +1,4 @@
 locals {
-  mongodb_dns = "${var.name}-headless.${var.namespace}.svc"
+  mongodb_dns = "${var.name}-headless.${var.namespace}.svc.cluster.local"
   mongodb_url = "mongodb+srv://${local.mongodb_dns}/${jsondecode(helm_release.mongodb.metadata[0].values).auth.databases[0]}"
 }

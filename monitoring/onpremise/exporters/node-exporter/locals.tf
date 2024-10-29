bed36ed7b56ced87a8808b8bd44daf05022c30f5
Junior Dongo <jdongo@aneo.fr>
2024-10-29 12:01:51 +0100

    fix: node_selector field for node_exporter

--- monitoring/onpremise/exporters/node-exporter/locals.tf
+++ monitoring/onpremise/exporters/node-exporter/locals.tf
@@ -1,4 +1,5 @@
 locals {
-  node_selector_keys   = keys(var.node_selector)
-  node_selector_values = values(var.node_selector)
+  node_selector_keys          = keys(var.node_selector)
+  node_selector_values        = values(var.node_selector)
+  node_exporter_node_selector = try(var.node_selector, {})
 }

b8581e1ab12ad13e5e4da425b0f1eae4be3fd2f5
nico_dreylaq <ndreyfuslaquieze@aneo.fr>
2025-01-17 10:29:00 +0100

    test: testing conf for mongo with tls

--- storage/onpremise/mongodb/locals.tf
+++ storage/onpremise/mongodb/locals.tf
@@ -1,4 +1,7 @@
 locals {
   mongodb_dns = "${var.name}-headless.${var.namespace}.svc.cluster.local"
   mongodb_url = "mongodb+srv://${local.mongodb_dns}/${jsondecode(helm_release.mongodb.metadata[0].values).auth.databases[0]}"
+    mongodb_root_password = data.kubernetes_secret.mongodb_credentials.data["mongodb-root-password"]
+  mongodb_extra_flags   = ["--tlsMode=allowTLS", "--tlsCertificateKeyFile=/mongodb/mongodb.pem", "--tlsCAFile=/mongodb/chain.pem", "--tlsAllowConnectionsWithoutCertificates"]
+
 }
--- storage/onpremise/mongodb/secrets.tf
+++ storage/onpremise/mongodb/secrets.tf
@@ -1,22 +1,49 @@
-data "kubernetes_secret" "mongodb_certificates" {
+data "kubernetes_secret" "mongodb_credentials" {
   metadata {
-    name      = "${helm_release.mongodb.name}-ca"
+    name      = helm_release.mongodb.name
     namespace = var.namespace
   }
+  depends_on = [helm_release.mongodb]
 }
+
+# resource "kubernetes_secret" "mongodb_admin" {
+#   metadata {
+#     name      = "${var.name}-admin"
+#     namespace = var.namespace
+#   }
+#   data = {
+#     MONGO_USERNAME = "root"
+#     MONGO_PASSWORD = local.mongodb_root_password
+#   }
+# }
+
+# resource "kubernetes_secret" "mongodb_user" {
+#   metadata {
+#     name      = "${var.name}-user"
+#     namespace = var.namespace
+#   }
+#   data = {
+#     # When using Bitnami's MongoDB image, MONGODB_USERNAME and MONGODB_PASSWORD are forbidden env variables names, see https://github.com/bitnami/containers/issues/18506
+#     MONGO_USERNAME = random_string.mongodb_application_user.result
+#     MONGO_PASSWORD = random_password.mongodb_application_password.result
+#   }
+# }
 
 resource "kubernetes_secret" "mongodb" {
   metadata {
-    name      = "mongodb"
-    namespace = var.namespace
+    name      = "custom-${var.name}"
+    namespace = helm_release.mongodb.namespace
   }
   data = {
-    "chain.pem"        = data.kubernetes_secret.mongodb_certificates.data["mongodb-ca-cert"]
+    "ca.pem"           = tls_self_signed_cert.root_mongodb.cert_pem
+    "mongodb.pem"      = format("%s\n%s", tls_locally_signed_cert.mongodb_certificate.cert_pem, tls_private_key.mongodb_private_key.private_key_pem)
+    "chain.pem"        = format("%s\n%s", tls_locally_signed_cert.mongodb_certificate.cert_pem, tls_self_signed_cert.root_mongodb.cert_pem)
     username           = random_string.mongodb_application_user.result
     password           = random_password.mongodb_application_password.result
     host               = local.mongodb_dns
-    port               = 27017
+    port               = var.mongodb.service_port
     url                = local.mongodb_url
-    number_of_replicas = local.replicas
+    number_of_replicas = var.sharding.shards.replicas
+    number_of_shards   = var.sharding.shards.quantity
   }
 }

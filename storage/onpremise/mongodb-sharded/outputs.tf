b7cfeaf413ae313c8a999e613603a67835757466
nico_dreylaq <ndreyfuslaquieze@aneo.fr>
2025-01-24 16:39:54 +0100

    fix: implem dns mongo nosharded

--- storage/onpremise/mongodb-sharded/outputs.tf
+++ storage/onpremise/mongodb-sharded/outputs.tf
@@ -5,7 +5,7 @@ output "host" {
 
 output "port" {
   description = "Port of MongoDB server"
-  value       = var.mongodb.service_port
+  value       = 27017
 }
 
 output "url" {
@@ -14,13 +14,8 @@ output "url" {
 }
 
 output "number_of_replicas" {
-  description = "Number of replicas for each shard"
-  value       = var.sharding.shards.replicas
-}
-
-output "number_of_shards" {
-  description = "Number of MongoDB shards"
-  value       = var.sharding.shards
+  description = "Number of replicas of MongoDB"
+  value       = var.mongodb.replicas
 }
 
 output "user_credentials" {
@@ -39,21 +34,29 @@ output "endpoints" {
   }
 }
 
+# Uses the unused variables that are defined to pass the CI pre-commit check
+output "unused_variables" {
+  description = "Map of variables that are not used yet but might be in the future"
+  value = {
+    "validity_period_hours" = var.validity_period_hours
+  }
+}
+
 #new Outputs 
 output "env" {
   description = "Elements to be set as environment variables"
   value = ({
     "Components__TableStorage"  = "ArmoniK.Adapters.MongoDB.TableStorage"
     "MongoDB__Host"             = local.mongodb_dns
-    "MongoDB__Port"             = var.mongodb.service_port
+    "MongoDB__Port"             = "27017"
     "MongoDB__Tls"              = "true"
     "MongoDB__ReplicaSet"       = "rs0"
     "MongoDB__DatabaseName"     = "database"
-    "MongoDB__DirectConnection" = "true"
-    "MongoDB__CAFile"           = "/mongodb/certs/ca.pem"
-    "MongoDB__Sharding"         = "true"
-    "MongoDB__AuthSource"       = "admin"
+    "MongoDB__DirectConnection" = "false"
+    "MongoDB__CAFile"           = "/mongodb/certificate/mongodb-ca-cert"
+    "MongoDB__AuthSource"       = "database"
   })
+
 }
 
 output "mount_secret" {
@@ -63,6 +66,11 @@ output "mount_secret" {
       secret = kubernetes_secret.mongodb.metadata[0].name
       path   = "/mongodb/certs/"
       mode   = "0644"
+    },
+    "mongo-certificate-helm" = {
+      secret = "${helm_release.mongodb.name}-ca"
+      path   = "/mongodb/certificate/"
+      mode   = "0644"
     }
   }
 }
@@ -71,12 +79,12 @@ output "env_from_secret" {
   description = "Environment variables from secrets"
   value = {
     "MongoDB__User" = {
-      secret = kubernetes_secret.mongodb_admin.metadata[0].name
-      field  = "MONGO_USERNAME"
+      secret = kubernetes_secret.mongodb_user.metadata[0].name
+      field  = "username"
     },
     "MongoDB__Password" = {
-      secret = kubernetes_secret.mongodb_admin.metadata[0].name
-      field  = "MONGO_PASSWORD"
+      secret = kubernetes_secret.mongodb_user.metadata[0].name
+      field  = "password"
     }
   }
 }
--- storage/onpremise/mongodb/secrets.tf
+++ storage/onpremise/mongodb/secrets.tf
@@ -5,20 +5,18 @@ data "kubernetes_secret" "mongodb_certificates" {
   }
 }
 
-# resource "kubernetes_secret" "mongodb" {
-#   metadata {
-#     name      = "mongodb"
-#     namespace = var.namespace
-#   }
-#   data = {
-#     "ca.pem"           = tls_self_signed_cert.root_mongodb.cert_pem
-#     "mongodb.pem"      = format("%s\n%s", tls_locally_signed_cert.mongodb_certificate.cert_pem, tls_private_key.mongodb_private_key.private_key_pem)
-#     "chain.pem"        = format("%s\n%s", tls_locally_signed_cert.mongodb_certificate.cert_pem, tls_self_signed_cert.root_mongodb.cert_pem)
-#     username           = random_string.mongodb_application_user.result
-#     password           = random_password.mongodb_application_password.result
-#     host               = local.mongodb_dns
-#     port               = 27017
-#     url                = local.mongodb_url
-#     number_of_replicas = local.replicas
-#   }
-# }
+resource "kubernetes_secret" "mongodb" {
+  metadata {
+    name      = "mongodb"
+    namespace = var.namespace
+  }
+  data = {
+    "chain.pem"        = data.kubernetes_secret.mongodb_certificates.data["mongodb-ca-cert"]
+    username           = random_string.mongodb_application_user.result
+    password           = random_password.mongodb_application_password.result
+    host               = local.mongodb_dns
+    port               = 27017
+    url                = local.mongodb_url
+    number_of_replicas = local.replicas
+  }
+}

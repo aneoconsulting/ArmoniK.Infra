7e14d52894263af1bcd5879fff348de32dbff87b
Florian Lemaitre <flemaitre@aneo.fr>
2025-03-20 23:41:06 +0100

    Expose prefix for PubSub

--- storage/gcp/pubsub/variables.tf
+++ storage/gcp/pubsub/variables.tf
@@ -7,3 +7,9 @@ variable "kms_key_id" {
   description = "Id of the crypto KMS used to encrypt/decrypt resources"
   type        = string
 }
+
+variable "prefix" {
+  description = "Prefix of the topic and subscription name"
+  type        = string
+  default     = ""
+}

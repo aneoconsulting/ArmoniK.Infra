dfa3f180bd79e65f2b26aa3b24dd37b86d018708
Florian Lemaitre <flemaitre@aneo.fr>
2026-02-17 11:49:18 +0100

    Make default images support any components instead of predefined ones

--- utils/default-images/outputs.tf
+++ utils/default-images/outputs.tf
@@ -5,32 +5,10 @@ output "armonik_versions" {
 
 output "image_tags" {
   description = "Tags of images used"
-  value = merge({
-    for image in var.armonik_images.infra :
-    image => var.armonik_versions.infra
-    }, {
-    for image in var.armonik_images.infra_plugins :
-    image => var.armonik_versions.infra_plugins
-    }, {
-    for image in var.armonik_images.core :
-    image => var.armonik_versions.core
-    }, {
-    for image in var.armonik_images.api :
-    image => var.armonik_versions.api
-    }, {
-    for image in var.armonik_images.gui :
-    image => var.armonik_versions.gui
-    }, {
-    for image in var.armonik_images.extcsharp :
-    image => var.armonik_versions.extcsharp
-    }, {
-    for image in var.armonik_images.extcpp :
-    image => var.armonik_versions.extcpp
-    }, {
-    for image in var.armonik_images.extjava :
-    image => var.armonik_versions.extjava
-    }, {
-    for image in var.armonik_images.samples :
-    image => var.armonik_versions.samples
-  }, var.image_tags)
+  value = merge(concat(
+    [for component, images in var.armonik_images :
+      { for image in images : image => var.armonik_versions[component] }
+    ],
+    [var.image_tags]
+  )...)
 }

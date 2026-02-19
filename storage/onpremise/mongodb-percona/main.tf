27a820cef5db64fe5bf160cbab4091be4b5280c0
sleep <abde.arrahman.dk@gmail.com>
2026-02-19 10:22:54 +0100

    Swapped to manifest for cluster with wait condition

--- storage/onpremise/mongodb-percona/main.tf
+++ storage/onpremise/mongodb-percona/main.tf
@@ -22,107 +22,84 @@ resource "helm_release" "operator" {
   ]
 
 }
-
-resource "helm_release" "cluster" {
-  name       = local.cluster_release_name
-  namespace  = var.namespace
-  chart      = var.cluster.helm_chart_name
-  repository = var.cluster.helm_chart_repository
-  version    = var.cluster.helm_chart_version
-  timeout    = var.timeout
-  wait = true
-  wait_for_jobs = true
-  # Wait for the operator to be ready before creating the CR
+resource "kubernetes_manifest" "cluster" {
   depends_on = [helm_release.operator]
-  values = [
-    yamlencode({
-      # >> Disabled features we don't need yet <<
+
+  manifest = {
+    apiVersion = "psmdb.percona.com/v1"
+    kind       = "PerconaServerMongoDB"
+    metadata = {
+      name      = local.cluster_release_name
+      namespace = var.namespace
+    }
+    spec = {
       backup = {
         enabled = false
+        image   = "percona/percona-backup-mongodb:2.12.0"
       }
 
-      finalizers = [] # TODO (NOT GOOD I THINK BUT IDK WHAT TO DO): otherwise I need to patch this in wheneve I destroy cuz it hangs... end me
-
-      # >> Sharding configuration <<
-      sharding = {
-        enabled   = var.sharding != null ? var.sharding.enabled : false
-
-        configsvrReplSet = var.sharding != null && var.sharding.enabled ? {
-          size = var.sharding.configsvr.replicas
-
-          volumeSpec = {
-            pvc = {
-              resources = {
-                requests = {
-                  storage = var.persistence.storage_size
-                }
-              }
-            }
-          }
-        } : {
-            size = 0
-            volumeSpec = {
-              pvc = {
-                resources = {
-                  requests = {
-                    storage = "1Gi"
-                  }
-                }
-              }
-            }
-          }
-
-        mongos = var.sharding != null && var.sharding.enabled ? {
-          size = var.sharding.mongos.replicas
-
-        } : {
-            size = 0
-          }
-      }
       allowUnsafeConfigurations = true
       unsafeFlags = {
-        replsetSize = var.cluster.replicas < 3 
-        # tls         = true
+        replsetSize = var.cluster.replicas < 3
         mongosSize  = var.sharding != null && var.sharding.enabled ? var.sharding.mongos.replicas < 2 : false
       }
 
-      # >> TLS configuration <<
       tls = {
-        mode = "allowTLS"
+        mode                     = "preferTLS"
         allowInvalidCertificates = true
       }
 
-      # >> Image configuration <<
-      image = merge(
-        { repository = var.cluster.image },
-        var.cluster.tag != null ? { tag = var.cluster.tag } : {}
-      )
+      image = var.cluster.tag != null ? "${var.cluster.image}:${var.cluster.tag}" : var.cluster.image
 
-      # >> User configuration <<
       users = [
         {
-        name = "application_user"
-        db = var.cluster.database_name
-        passwordSecretRef = {
+          name = "application_user"
+          db   = var.cluster.database_name
+          passwordSecretRef = {
             name = "application-user-password"
             key  = "password"
           }
-        roles = [{
-          name = "readWrite"
-          db = var.cluster.database_name
-        },
-        {
-          name = "dbAdmin"
-          db = var.cluster.database_name
-        },
-        { name = "clusterAdmin", db = "admin" } # I do not like the way this is but this is necessary for sharding on the ArmoniK.Core side of things..
-        ]
-      }
+          roles = [
+            { name = "readWrite", db = var.cluster.database_name },
+            { name = "dbAdmin", db = var.cluster.database_name },
+            { name = "clusterAdmin", db = "admin" },
+          ]
+        }
       ]
 
-      # >> Replica Set configuration <<
-      replsets = {
-        rs0 = {
+      sharding = {
+        enabled = var.sharding != null ? var.sharding.enabled : false
+
+        configsvrReplSet = var.sharding != null && var.sharding.enabled ? {
+          size = var.sharding.configsvr.replicas
+          volumeSpec = {
+            persistentVolumeClaim = {
+              resources = {
+                requests = { storage = var.persistence.storage_size }
+              }
+            }
+          }
+        } : {
+          size = 0
+          volumeSpec = {
+            persistentVolumeClaim = {
+              resources = {
+                requests = { storage = "1Gi" }
+              }
+            }
+          }
+        }
+
+        mongos = var.sharding != null && var.sharding.enabled ? {
+          size = var.sharding.mongos.replicas
+        } : {
+          size = 0
+        }
+      }
+
+      replsets = [
+        {
+          name = "rs0"
           size = var.cluster.replicas
 
           affinity = {
@@ -130,7 +107,7 @@ resource "helm_release" "cluster" {
           }
 
           nodeSelector = var.cluster.node_selector
-          # will have to do this for shards/cfgsvrs but I'll do it later (kill me)
+
           tolerations = [
             for key, value in var.cluster.node_selector : {
               key      = key
@@ -139,10 +116,11 @@ resource "helm_release" "cluster" {
               effect   = "NoSchedule"
             }
           ]
-          resources    = var.resources.shards
+
+          resources = var.resources.shards
 
           volumeSpec = {
-            pvc = {
+            persistentVolumeClaim = {
               storageClassName = try(
                 coalesce(var.persistence.shards.storage_class_name),
                 length(kubernetes_storage_class.shards) > 0 ? kubernetes_storage_class.shards[0].metadata[0].name : null,
@@ -156,7 +134,19 @@ resource "helm_release" "cluster" {
             }
           }
         }
-      }
-    })
-  ]
+      ]
+    }
+  }
+
+  wait {
+    condition {
+      type   = "ready"
+      status = "True"
+    }
+  }
+
+  timeouts {
+    create = "15m"
+    update = "15m"
+  }
 }
